e77c212827365423e6b7aee717c2bddae84ef001
Daniel Lemire <lemire@gmail.com>
2018-12-24 16:44:42 -0500

    Adding another script.

---
+++ scripts/dumpsimplestats.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $SCRIPTPATH/..
+make jsonstats
+echo 
+for i in $SCRIPTPATH/../jsonexamples/*.json; do
+    [ -f "$i" ] || break
+    echo $i
+    $SCRIPTPATH/../jsonstats $i
+    echo
+done
