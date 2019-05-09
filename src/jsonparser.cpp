870112f2f350896e568deef21924bcbc2734e48d
Daniel Lemire <lemire@gmail.com>
2019-05-08 20:47:56 -0400

    Revisiting faulty logic.

--- src/jsonparser.cpp
+++ src/jsonparser.cpp
@@ -21,9 +21,16 @@ int json_parse(const uint8_t *buf, size_t len, ParsedJson &pj, bool reallocifnee
 	  GetSystemInfo(&sysInfo); 
 	  long pagesize = sysInfo.dwPageSize;
 #else
-     long pagesize = sysconf (_SC_PAGESIZE); 
+    long pagesize = sysconf (_SC_PAGESIZE); 
 #endif
-	 if ( (reinterpret_cast<uintptr_t>(buf + len - 1) % pagesize ) < SIMDJSON_PADDING ) {
+  //////////////
+  // We want to check that buf + len - 1 and buf + len - 1 + SIMDJSON_PADDING
+  // are in the same page.
+  // That is, we want to check that  
+  // (buf + len - 1) / pagesize == (buf + len - 1 + SIMDJSON_PADDING) / pagesize
+  // That's true if (buf + len - 1) % pagesize + SIMDJSON_PADDING < pagesize.
+  ///////////
+	 if ( (reinterpret_cast<uintptr_t>(buf + len - 1) % pagesize ) + SIMDJSON_PADDING < static_cast<uintptr_t>(pagesize) ) {
        const uint8_t *tmpbuf  = buf;
        buf = (uint8_t *) allocate_padded_buffer(len);
        if(buf == NULL) return simdjson::MEMALLOC;
