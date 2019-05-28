f00be3031838f53a6686d5c8457ba48694e18fef
Daniel Lemire <lemire@gmail.com>
2019-05-28 19:32:56 -0400

    Being clearer as to what TAPE_ERROR means.

--- include/simdjson/simdjson.h
+++ include/simdjson/simdjson.h
@@ -8,7 +8,7 @@ struct simdjson {
     SUCCESS = 0,
     CAPACITY, // This ParsedJson can't support a document that big
     MEMALLOC, // Error allocating memory, most likely out of memory
-    TAPE_ERROR, // Something went wrong while writing to the tape
+    TAPE_ERROR, // Something went wrong while writing to the tape (stage 2), this is a generic error
     DEPTH_ERROR, // Your document exceeds the user-specified depth limitation
     STRING_ERROR, // Problem while parsing a string
     T_ATOM_ERROR, // Problem while parsing an atom starting with the letter 't'
