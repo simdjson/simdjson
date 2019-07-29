3477ddb439e3a6a2e595c07ea778a772b15ce8b2
Daniel Lemire <lemire@gmail.com>
2019-07-29 13:22:33 -0400

    Let us not confuse the issue.

--- include/simdjson/numberparsing.h
+++ include/simdjson/numberparsing.h
@@ -276,17 +276,12 @@ parse_float(const uint8_t *const buf,
       expnumber = 10 * expnumber + digit;
       ++p;
     }
-    while (is_integer(*p)) {
-      if(expnumber > 0x100000000) {// we need to check for overflows
+    if (is_integer(*p)) {
 // we refuse to parse this
 #ifdef JSON_TEST_NUMBERS // for unit testing
-        foundInvalidNumber(buf + offset);
+      foundInvalidNumber(buf + offset);
 #endif
-        return false;
-      }
-      digit = *p - '0';
-      expnumber = 10 * expnumber + digit;
-      ++p;      
+      return false;
     }
     if (unlikely(expnumber > 308)) {
       // this path is unlikely
@@ -519,17 +514,12 @@ static really_inline bool parse_number(const uint8_t *const buf,
       expnumber = 10 * expnumber + digit;
       ++p;
     }
-    while (is_integer(*p)) {
-      if(expnumber > 0x100000000) {// we need to check for overflows
+    if (is_integer(*p)) {
 // we refuse to parse this
 #ifdef JSON_TEST_NUMBERS // for unit testing
-        foundInvalidNumber(buf + offset);
+      foundInvalidNumber(buf + offset);
 #endif
-        return false;
-      }
-      digit = *p - '0';
-      expnumber = 10 * expnumber + digit;
-      ++p;      
+      return false;
     }
     exponent += (negexp ? -expnumber : expnumber);
   }
