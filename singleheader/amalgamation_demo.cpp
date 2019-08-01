ee66fb1c602e17563606c6f6eecc225dac5455cc
Daniel Lemire <lemire@gmail.com>
2019-08-01 16:23:30 -0400

    Version 0.2.0.

--- singleheader/amalgamation_demo.cpp
+++ singleheader/amalgamation_demo.cpp
@@ -1,13 +1,16 @@
-/* auto-generated on Sun 28 Jul 2019 18:08:49 EDT. Do not edit! */
+/* auto-generated on Thu  1 Aug 2019 16:18:07 EDT. Do not edit! */
 
 #include <iostream>
 #include "simdjson.h"
 #include "simdjson.cpp"
 int main(int argc, char *argv[]) {
+  if(argc < 2) {
+    std::cerr << "Please specify a filename " << std::endl;
+  }
   const char * filename = argv[1];
   simdjson::padded_string p = simdjson::get_corpus(filename);
   simdjson::ParsedJson pj = simdjson::build_parsed_json(p); // do the parsing
-  if( ! pj.isValid() ) {
+  if( ! pj.is_valid() ) {
     std::cout << "not valid" << std::endl;
   } else {
     std::cout << "valid" << std::endl;
