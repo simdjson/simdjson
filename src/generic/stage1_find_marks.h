b65173e9722a8cc709ce754aebf06701a7b182dc
John Keiser <john@johnkeiser.com>
2019-08-18 11:11:10 -0700

    Fix stage1_find_marks on amd64

--- src/generic/stage1_find_marks.h
+++ src/generic/stage1_find_marks.h
@@ -3,19 +3,6 @@
 // We assume the file in which it is include already includes
 // "simdjson/stage1_find_marks.h" (this simplifies amalgation)
 
-namespace {
-// for when clmul is unavailable
-[[maybe_unused]] uint64_t portable_compute_quote_mask(uint64_t quote_bits) {
-  uint64_t quote_mask = quote_bits ^ (quote_bits << 1);
-  quote_mask = quote_mask ^ (quote_mask << 2);
-  quote_mask = quote_mask ^ (quote_mask << 4);
-  quote_mask = quote_mask ^ (quote_mask << 8);
-  quote_mask = quote_mask ^ (quote_mask << 16);
-  quote_mask = quote_mask ^ (quote_mask << 32);
-  return quote_mask;
-}
-} // namespace
-
 // return a bitvector indicating where we have characters that end an odd-length
 // sequence of backslashes (and thus change the behavior of the next character
 // to follow). A even-length sequence of backslashes, and, for that matter, the
--- src/stage1_find_marks.cpp
+++ src/stage1_find_marks.cpp
@@ -1,10 +1,18 @@
 #include "simdjson/portability.h"
 
-#ifdef IS_X86_64
+namespace {
+// for when clmul is unavailable
+[[maybe_unused]] uint64_t portable_compute_quote_mask(uint64_t quote_bits) {
+  uint64_t quote_mask = quote_bits ^ (quote_bits << 1);
+  quote_mask = quote_mask ^ (quote_mask << 2);
+  quote_mask = quote_mask ^ (quote_mask << 4);
+  quote_mask = quote_mask ^ (quote_mask << 8);
+  quote_mask = quote_mask ^ (quote_mask << 16);
+  quote_mask = quote_mask ^ (quote_mask << 32);
+  return quote_mask;
+}
+} // namespace
+
+#include "arm64/stage1_find_marks.h"
 #include "haswell/stage1_find_marks.h"
 #include "westmere/stage1_find_marks.h"
-#endif // IS_X86_64
-
-#ifdef IS_ARM64
-#include "arm64/stage1_find_marks.h"
-#endif // IS_ARM64
