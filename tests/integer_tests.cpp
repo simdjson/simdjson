798d70c22ff1a915b9a4bc59c41c753aa2da2f5c
saka1 <github@saka1.net>
2019-08-19 18:31:50 +0000

    Brush up integer_tests.cpp

--- tests/integer_tests.cpp
+++ tests/integer_tests.cpp
@@ -7,19 +7,19 @@
 
 using namespace simdjson;
 
-const std::string make_json(const std::string value) {
+static const std::string make_json(const std::string value) {
   const std::string s = "{\"key\": ";
   return s + value + "}";
 }
 
 // e.g. make_json(123) => {"key": 123} as string
-template <typename T> const std::string make_json(T value) {
+template <typename T> static const std::string make_json(T value) {
   return make_json(std::to_string(value));
 }
 
 template <typename T>
-void parse_and_validate(const std::string src, T expected) {
-  std::cout << "src: " << src << std::endl;
+static void parse_and_validate(const std::string src, T expected) {
+  std::cout << "src: " << src << ", ";
   const padded_string pstr{src};
   auto json = build_parsed_json(pstr);
 
@@ -29,31 +29,28 @@ void parse_and_validate(const std::string src, T expected) {
   assert(it.next());
   bool result;
   if constexpr (std::is_same<int64_t, T>::value) {
-    int64_t actual = it.get_integer();
+    const auto actual = it.get_integer();
     result = expected == actual;
-    std::cout << "e:" << expected << ", " << actual << ", " << result
-              << std::endl;
   } else {
-    uint64_t actual = it.get_unsigned_integer();
+    const auto actual = it.get_unsigned_integer();
     result = expected == actual;
-    std::cout << "e:" << expected << ", " << actual << ", " << result
-              << std::endl;
   }
+  std::cout << std::boolalpha << "test: " << result << std::endl;
   assert(result);
-  if (!result) {
-    std::cerr << "faild to compare!!";
-  }
 }
 
 int main() {
   using std::numeric_limits;
-#if 0
-  constexpr int64_t int64_max = numeric_limits<int64_t>::max();
-  parse_and_validate(make_json(int64_max), int64_max);
+  constexpr auto int64_max = numeric_limits<int64_t>::max();
   constexpr auto int64_min = numeric_limits<int64_t>::min();
-  parse_and_validate(make_json(int64_min), int64_min);
-#endif
   constexpr auto uint64_max = numeric_limits<uint64_t>::max();
+  constexpr auto uint64_min = numeric_limits<uint64_t>::min();
+  parse_and_validate(make_json(int64_max), int64_max);
+  parse_and_validate(make_json(int64_min), int64_min);
   parse_and_validate(make_json(uint64_max), uint64_max);
+  parse_and_validate(make_json(uint64_min), uint64_min);
+
+  constexpr auto int64_max_plus1 = static_cast<uint64_t>(int64_max) + 1;
+  parse_and_validate(make_json(int64_max_plus1), int64_max_plus1);
 }
 
