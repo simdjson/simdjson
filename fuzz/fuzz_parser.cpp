05ad677e283e836d12f20b6f81a9e2aa03f113d3
Paul Dreik <github@pauldreik.se>
2019-10-25 00:05:19 +0200

    add new fuzzer for minification, simplify the old one

---
+++ fuzz/fuzz_parser.cpp
@@ -0,0 +1,10 @@
+#include "simdjson/jsonparser.h"
+#include <cstddef>
+#include <cstdint>
+#include <string>
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+
+   auto ignored= simdjson::build_parsed_json(Data,Size);
+
+  return 0;
+}
