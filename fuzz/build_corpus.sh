1d696b9280268ad7c591f2ac98424878b1812bb2
Paul Dreik <github@pauldreik.se>
2019-10-30 09:39:16 +0100

    fix zip command

--- fuzz/build_corpus.sh
+++ fuzz/build_corpus.sh
@@ -9,12 +9,12 @@ tmp=$(mktemp -d)
 root=$(readlink -f "$(dirname "$0")/..")
 
 find $root -type f -name "*.json" | while read -r json; do
- echo json=$json
+ # echo json=$json
  cp "$json" "$tmp"/$(sha1sum < "$json" |cut -f1 -d' ').json
 done
 
 echo $tmp
-zip --junk-paths corpus.zip "$tmp"
+zip --junk-paths -r corpus.zip "$tmp"
 rm -rf "$tmp"
 
 
