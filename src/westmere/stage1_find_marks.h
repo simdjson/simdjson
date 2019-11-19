7d7bec856d5cbb9f4907787935e813e8e8b66fcd
John Keiser <john@johnkeiser.com>
2019-11-19 10:29:25 -0800

    Remove lookup_lower_4_bits
    It's only a coincidence that it works in current uses: it doesn't do
    what the name says. Particularly, if the high bit is 1 it will yield
    0 even if the lower 4 bits would yield something else.

--- src/westmere/stage1_find_marks.h
+++ src/westmere/stage1_find_marks.h
@@ -18,12 +18,18 @@ really_inline void find_whitespace_and_operators(
   const simd8x64<uint8_t> in,
   uint64_t &whitespace, uint64_t &op) {
 
+  // These lookups rely on the fact that anything < 127 will match the lower 4 bits, which is why
+  // we can't use the generic lookup_16.
+  auto whitespace_table = simd8<uint8_t>::repeat_16(' ', 100, 100, 100, 17, 100, 113, 2, 100, '\t', '\n', 112, 100, '\r', 100, 100);
+  auto op_table = simd8<uint8_t>::repeat_16(',', '}', 0, 0, 0xc0u, 0, 0, 0, 0, 0, 0, 0, 0, 0, ':', '{');
+
   whitespace = in.map([&](simd8<uint8_t> _in) {
-    return _in == _in.lookup_lower_4_bits<uint8_t>(' ', 100, 100, 100, 17, 100, 113, 2, 100, '\t', '\n', 112, 100, '\r', 100, 100);
+    return _in == simd8<uint8_t>(_mm_shuffle_epi8(whitespace_table, _in));
   }).to_bitmask();
 
   op = in.map([&](simd8<uint8_t> _in) {
-    return (_in | 32) == (_in+0xd4u).lookup_lower_4_bits<uint8_t>(',', '}', 0, 0, 0xc0u, 0, 0, 0, 0, 0, 0, 0, 0, 0, ':', '{');
+    // | 32 handles the fact that { } and [ ] are exactly 32 bytes apart
+    return (_in | 32) == simd8<uint8_t>(_mm_shuffle_epi8(op_table, _in-','));
   }).to_bitmask();
 }
 
