c9882e144282ee0ad83da468bfa261fa88c90c04
Daniel Lemire <lemire@gmail.com>
2019-12-14 15:13:06 -0500

    Typo

--- src/haswell/bitmask.h
+++ src/haswell/bitmask.h
@@ -17,7 +17,7 @@ namespace simdjson::haswell {
 // For example, prefix_xor(00100100) == 00011100
 //
 really_inline uint64_t prefix_xor(const uint64_t bitmask) {
-  // There should be no such thing with a processing supporting avx2
+  // There should be no such thing with a processor supporting avx2
   // but not clmul.
   __m128i all_ones = _mm_set1_epi8('\xFF');
   __m128i result = _mm_clmulepi64_si128(_mm_set_epi64x(0ULL, bitmask), all_ones, 0);
