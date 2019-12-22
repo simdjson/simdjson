5f84d195cdc458f4d0d5aae8d1e5163afc7db7ca
dbjdbj <dbj@dbj.org>
2019-12-22 09:47:56 +0100

    nanolib namespace  + inside simjson temporarily for pull R to pass the CI

--- include/simdjson/simdjson.h
+++ include/simdjson/simdjson.h
@@ -2,8 +2,56 @@
 #define SIMDJSON_SIMDJSON_H
 
 #include <string>
-#include "simdjson_nanolib.h"
 #include "isadetection.h"
+
+namespace simdjson {
+  namespace nanolib {
+  //
+  // compile time native string handling -- C++11 or better
+  //
+  // NOTE: for compiler time, arguments have to be compile time values
+  // that is: literals or constexpr values
+  // for testing use https://godbolt.org/z/gGL95T
+  //
+  // NOTE: C++20 char8_t is a path peppered with a shards of glass, just don't
+  // go there
+  //
+  // NOTE: char16_t and char32_t are ok. if need them add them bellow
+  //
+  // NOTE: WIN32 is UTF-16 aka wchar_t universe, WIN32 char API's are all
+  // translated to wchar_t
+  //
+  constexpr bool strings_equal(char const *lhs, char const *rhs) noexcept {
+    while (*lhs || *rhs)
+      if (*lhs++ != *rhs++)
+        return false;
+    return true;
+  }
+
+  constexpr bool strings_equal(wchar_t const *lhs,
+                               wchar_t const *rhs) noexcept {
+    while (*lhs || *rhs)
+      if (*lhs++ != *rhs++)
+        return false;
+    return true;
+  }
+
+  constexpr size_t ct_strlen(const char *ch) noexcept {
+    size_t len = 0;
+    while (ch[len])
+      ++len;
+    return len;
+  }
+
+  constexpr size_t ct_strlen(const wchar_t *ch) noexcept {
+    size_t len = 0;
+    while (ch[len])
+      ++len;
+    return len;
+  }
+  } // namespace nanolib
+} // simdjson
+
 
 namespace simdjson {
 // Represents the minimal architecture that would support an implementation
@@ -22,6 +70,105 @@ enum class Architecture : unsigned short {
 #endif
 };
 
+/*
+NOTE: for compile time result, argument has to be compile time value
+
+constexpr Architecture arch_code_ = parse_architecture("ARM64") ;
+*/
+constexpr inline Architecture parse_architecture(char const *architecture) {
+    
+    using namespace simdjson::nanolib ;
+
+  if (strings_equal(architecture, "HASWELL")) {
+    return Architecture::HASWELL;
+  }
+  if (strings_equal(architecture, "WESTMERE")) {
+    return Architecture::WESTMERE;
+  }
+  if (strings_equal(architecture, "ARM64")) {
+    return Architecture::ARM64;
+  }
+  return Architecture::UNSUPPORTED;
+}
+
+/*
+constexpr auto arch_name_ = parse_architecture(Architecture::ARM64) ;
+*/
+constexpr char const * architecture_name (Architecture code_) noexcept {
+  // C++11 -- single return statement
+  return (
+      code_ == Architecture::ARM64
+          ? "ARM64"
+          : (code_ == Architecture::HASWELL
+                 ? "HASWELL"
+                 : (code_ == Architecture::WESTMERE ? "WESTMERE" : "UNSUPPORTED")));
+}
+
+/*
+Compile time Architecture code (C++11). Example.
+
+// user defined literal type
+// leave all the default scafolding to the compiler
+struct parser_type final
+{
+    const Architecture code_ {} ;
+    // secret sauce is constexpr constructor
+    constexpr parser_type( Architecture a_)  : code_(a_) {}
+    constexpr Architecture arch_code () const noexcept { return code_ ; }
+    constexpr char const * arch_name () const noexcept { return architecture_name(code_) ; }
+} ;
+
+// using the above requires x_ to be template argument
+template<  Architecture x_ >
+void test_parser() 
+{
+    constexpr parser_type amd_parser{ x_ } ;
+    constexpr auto code = amd_parser.arch_code();
+    constexpr auto name = amd_parser.arch_name();
+
+    printf("\nFor architecture code %d, names is %s", code, name );
+}
+
+*/
+// a literal type holding the simdjson::Architecture code
+// one type --> many codes
+struct compile_time_architecture_code {
+  const Architecture arch_{};
+  constexpr compile_time_architecture_code(Architecture x_) : arch_(x_) {}
+  constexpr Architecture code() const noexcept { return arch_; }
+  constexpr char const *name() const noexcept {
+    return architecture_name(arch_) ;
+  }
+};
+
+// long descriptive name is important
+// shorcut is comfortable 
+// when in doubt what is simdjson::CTA 
+// one can always use IDE to find out
+    using CTA = compile_time_architecture_code;
+
+/*
+-----------------------------------------------------------------------
+simdjson::Architecture will not change at runtime.  So, one does not have to check 
+for it repeatedly, from constructors for example.
+
+If code_ arg is compile time one can make compile time CTA
+*/
+    constexpr CTA
+        make_cta(Architecture code_) {
+  // C++11 -- single return statement
+  return (code_ == Architecture::ARM64
+              ? CTA(Architecture::ARM64)
+              : (code_ == Architecture::HASWELL
+                     ? CTA(Architecture::HASWELL)
+                     : (code_ == Architecture::WESTMERE
+                            ? CTA(Architecture::WESTMERE)
+                            : CTA(Architecture::UNSUPPORTED))));
+}
+/*
+---------------------------------------------------------------------------
+if this function could be compile time, many simdjson  API's could be too
+*/
 inline Architecture find_best_supported_architecture() {
   constexpr uint32_t haswell_flags =
       instruction_set::AVX2 | instruction_set::PCLMULQDQ |
@@ -29,6 +176,8 @@ inline Architecture find_best_supported_architecture() {
   constexpr uint32_t westmere_flags =
       instruction_set::SSE42 | instruction_set::PCLMULQDQ;
 
+  // because of this call the whole of this function
+  // can not be compile time
   uint32_t supports = detect_supported_architectures();
   // Order from best to worst (within architecture)
   if ((haswell_flags & supports) == haswell_flags)
@@ -41,21 +190,7 @@ inline Architecture find_best_supported_architecture() {
   return Architecture::UNSUPPORTED;
 }
 
-/*
-NOTE: for compile time result, argument has to be compile time value
-*/
-constexpr inline Architecture parse_architecture(char const *architecture) {
-  if (strings_equal(architecture, "HASWELL")) {
-    return Architecture::HASWELL;
-  }
-  if (strings_equal(architecture, "WESTMERE")) {
-    return Architecture::WESTMERE;
-  }
-  if (strings_equal(architecture, "ARM64")) {
-    return Architecture::ARM64;
-  }
-  return Architecture::UNSUPPORTED;
-}
+
 
 /// <summary>
 /// call this once upon entering main() , if it returns false
--- include/simdjson/simdjson_nanolib.h
+++ include/simdjson/simdjson_nanolib.h
@@ -2,45 +2,50 @@
 #define _SIMDJSON_INC_NANOLIB_
 
 namespace simdjson {
+namespace nanolib {
 //
-// compile time string handling
-// C++11 or better
-// NOTE: for compiler time results, arguments have to be compile time values
+// compile time native string handling -- C++11 or better
+//
+// NOTE: for compiler time, arguments have to be compile time values
 // that is: literals or constexpr values
 // for testing use https://godbolt.org/z/gGL95T
 //
-// NOTE: C++20 char8_t is a path peppered with a shards of glass, just don't go there
-// NOTE: char16_t and char32_t are ok. if need them add them bellow
-// NOTE: WIN32 is UTF-16 aka wchar_t universe, WIN32 char API's are all translated to wchar_t
+// NOTE: C++20 char8_t is a path peppered with a shards of glass, just don't go 
+// there 
 //
-constexpr bool strings_equal(char const *lhs, char const *rhs) {
+// NOTE: char16_t and char32_t are ok. if need them add them bellow 
+//
+// NOTE: WIN32 is UTF-16 aka wchar_t universe, WIN32 char API's are all translated to
+// wchar_t
+//
+constexpr bool strings_equal(char const *lhs, char const *rhs) noexcept {
   while (*lhs || *rhs)
     if (*lhs++ != *rhs++)
       return false;
   return true;
 }
 
-constexpr bool strings_equal(wchar_t const *lhs, wchar_t const *rhs) {
+constexpr bool strings_equal(wchar_t const *lhs, wchar_t const *rhs) noexcept {
   while (*lhs || *rhs)
     if (*lhs++ != *rhs++)
       return false;
   return true;
 }
 
-constexpr size_t ct_strlen(const char *ch) {
+constexpr size_t ct_strlen(const char *ch) noexcept {
   size_t len = 0;
   while (ch[len])
     ++len;
   return len;
 }
 
-constexpr size_t ct_strlen(const wchar_t *ch) {
+constexpr size_t ct_strlen(const wchar_t *ch) noexcept {
   size_t len = 0;
   while (ch[len])
     ++len;
   return len;
 }
-
+} // namespace nanolib
 } // simdjson
 
 #endif // !_SIMDJSON_INC_NANOLIB_
