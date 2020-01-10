2397983ac19d22118bb3186dfff7b2e8af05a2c3
Daniel Lemire <lemire@gmail.com>
2020-01-10 15:33:30 -0500

    Instead of emulating the whole parsing as stage 1 + stage 2, let us benchmark the real thing.

--- benchmark/benchmarker.h
+++ benchmark/benchmarker.h
@@ -303,27 +303,35 @@ struct benchmarker {
       exit_error(string("Failed to parse ") + filename + " during stage 1: " + pj.get_error_message());
     }
 
-    // Stage 2 (unified machine)
-    event_count stage2_count;
-    if (!stage1_only || stats == NULL) {
-      if (!stage1_only) {
-        collector.start();
-      }
+    // Stage 2 (unified machine) and the rest
+
+    if (stage1_only) {
+      all_stages << stage1_count;
+    } else {
+      event_count stage2_count;
+      collector.start();
       result = parser.stage2((const uint8_t *)json.data(), json.size(), pj);
-      if (!stage1_only) {
-        stage2_count = collector.end();
-        stage2 << stage2_count;
-      }
-
+      stage2_count = collector.end();
+      stage2 << stage2_count;
+      // You would think that the entire processing is just stage 1 + stage 2, but
+      // empirically, that's not true! Not even close to be true in some instances.
+      event_count allstages_count;
+      collector.start();
+      result = parser.parse((const uint8_t *)json.data(), json.size(), pj);
       if (result != simdjson::SUCCESS) {
-        exit_error(string("Failed to parse ") + filename + " during stage 2: " + pj.get_error_message());
+        exit_error(string("Failed to parse ") + filename + " during overall parsing " + pj.get_error_message());
       }
+      allstages_count = collector.end();
+      all_stages << allstages_count;
     }
-
-    all_stages << (stage1_count + stage2_count);
-
     // Calculate stats the first time we parse
     if (stats == NULL) {
+      if (stage1_only) { //  we need stage 2 once
+        result = parser.stage2((const uint8_t *)json.data(), json.size(), pj);
+        if (result != simdjson::SUCCESS) {
+          printf("Warning: failed to parse during stage 2. Unable to acquire statistics.\n");
+        }
+      }
       stats = new json_stats(json, pj);
     }
   }
