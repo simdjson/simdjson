a98d8419835f4f185669d7c6008834c1c4dd430e
Daniel Lemire <lemire@gmail.com>
2020-02-24 17:13:10 -0500

    Minor tweaks. (#507)

--- amalgamation.sh
+++ amalgamation.sh
@@ -148,7 +148,9 @@ int main(int argc, char *argv[]) {
   simdjson::padded_string p = simdjson::get_corpus(filename);
   auto [doc, error] = simdjson::document::parse(p); // do the parsing
   if (error) {
-    std::cout << "document::parse not valid" << std::endl;
+    std::cout << "document::parse failed" << std::endl;
+    std::cout << "error code: " << error << std::endl;
+    std::cout << error_message(error) << std::endl;
   } else {
     std::cout << "document::parse valid" << std::endl;
   }
