b3706d3d830159612b4f512ada12171785d6edce
Daniel Lemire <lemire@gmail.com>
2020-03-30 14:49:50 -0400

    Some users are interested, as a metric, in the number of documents parsed per second. Obviously, this means reusing the same parser again and again.

--- benchmark/parse.cpp
+++ benchmark/parse.cpp
@@ -212,7 +212,7 @@ int main(int argc, char *argv[]) {
   if (!options.verbose) { progress.erase(); }
 
   for (size_t i=0; i<options.files.size(); i++) {
-    benchmarkers[i]->print(options.tabbed_output);
+    benchmarkers[i]->print(options.tabbed_output, options.iterations);
     delete benchmarkers[i];
   }
 
