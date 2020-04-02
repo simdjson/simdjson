13aee51011ef9452e5c2a006996f06fe4c8568b2
John Keiser <john@johnkeiser.com>
2020-04-02 12:14:29 -0700

    Add element.type() for type switching

--- tests/readme_examples.cpp
+++ tests/readme_examples.cpp
@@ -64,6 +64,52 @@ void basics_dom_2() {
   cout << cars.at("0/tire_pressure/1") << endl; // Prints 39.9}
 }
 
+namespace treewalk_1 {
+  void print_json(dom::element element) {
+    switch (element.type()) {
+      case dom::element_type::ARRAY:
+        cout << "[";
+        for (dom::element child : dom::array(element)) {
+          print_json(child);
+          cout << ",";
+        }
+        cout << "]";
+        break;
+      case dom::element_type::OBJECT:
+        cout << "{";
+        for (dom::key_value_pair field : dom::object(element)) {
+          cout << "\"" << field.key << "\": ";
+          print_json(field.value);
+        }
+        cout << "}";
+        break;
+      case dom::element_type::INT64:
+        cout << int64_t(element) << endl;
+        break;
+      case dom::element_type::UINT64:
+        cout << uint64_t(element) << endl;
+        break;
+      case dom::element_type::DOUBLE:
+        cout << double(element) << endl;
+        break;
+      case dom::element_type::STRING:
+        cout << std::string_view(element) << endl;
+        break;
+      case dom::element_type::BOOL:
+        cout << bool(element) << endl;
+        break;
+      case dom::element_type::NULL_VALUE:
+        cout << "null" << endl;
+        break;
+    }
+  }
+
+  void basics_treewalk_1() {
+    dom::parser parser;
+    print_json(parser.load("twitter.json"));
+  }
+}
+
 void basics_ndjson() {
   dom::parser parser;
   for (dom::element doc : parser.load_many("x.txt")) {
