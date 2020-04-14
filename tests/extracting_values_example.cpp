d9a1df7aff8522c9c01ac94164a4ac11003a63f8
Daniel Lemire <lemire@gmail.com>
2020-04-14 16:04:52 -0400

    Added a smaller piece of code

--- tests/extracting_values_example.cpp
+++ tests/extracting_values_example.cpp
@@ -2,19 +2,11 @@
 #include "simdjson.h"
 
 int main() {
-  auto numberstring = "1.2"_padded;
-  simdjson::dom::parser parser;
   simdjson::error_code error;
-  double value;
+  double value; // variable where we store the value to be parsed
+  simdjson::padded_string numberstring = "1.2"_padded; // our JSON input ("1.2")
+  simdjson::dom::parser parser;
   parser.parse(numberstring).get<double>().tie(value,error);
-  if(error) {
-    std::cerr << "error parsing JSON" << std::endl;
-    return EXIT_FAILURE;
-  }
-  if(value != 1.2) {
-    std::cerr << "bad value" << std::endl;
-    return EXIT_FAILURE;
-  }
+  if (error) { std::cerr << error << std::endl; return EXIT_FAILURE; }
   std::cout << "I parsed " << value << " from " << numberstring.data() << std::endl;
-  return EXIT_SUCCESS;
 }
