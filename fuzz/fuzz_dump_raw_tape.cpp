92c34f7f387e7a228fec6bba364fc91b6b42cfb0
Paul Dreik <github@pauldreik.se>
2020-04-14 07:45:30 +0200

    do not use deprecated apis in the fuzzers (#705)
    * move from deprecated interface in fuzz dump raw tape
    
    * update fuzz_dump to the non deprecated replacement
    
    * replace use of deprecated api
    
    * hopefully fix windows build

--- fuzz/fuzz_dump_raw_tape.cpp
+++ fuzz/fuzz_dump_raw_tape.cpp
@@ -8,11 +8,13 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 
-  try {
-    auto pj = simdjson::build_parsed_json(Data, Size);
-    NulOStream os;
-    UNUSED bool ignored=pj.dump_raw_tape(os);
-  } catch (...) {
-  }
-  return 0;
+    try {
+        simdjson::dom::parser pj;
+        auto elem=pj.parse(Data, Size);
+        auto v=elem.value();
+        NulOStream os;
+        UNUSED auto dumpstatus=v.dump_raw_tape(os);
+    } catch (...) {
+    }
+    return 0;
 }
