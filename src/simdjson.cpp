d40069a018ccd5bfb6fc1b19d2252abb64c074dc
Pavel P <pavlov.pavel@gmail.com>
2020-04-26 03:53:00 +0600

    Disable deprecation warnings for VS builds
    fopen/getenv are standard c++ that are not deprecated.

--- src/simdjson.cpp
+++ src/simdjson.cpp
@@ -1,11 +1,14 @@
 #include "simdjson.h"
 
+SIMDJSON_PUSH_DISABLE_WARNINGS
 #if defined(SIMDJSON_CLANG_VISUAL_STUDIO)
 SIMDJSON_DISABLE_GCC_WARNING(-Wmicrosoft-include)
 #endif
-
+SIMDJSON_DISABLE_DEPRECATED_WARNING
 
 #include "error.cpp"
 #include "implementation.cpp"
 #include "stage1_find_marks.cpp"
 #include "stage2_build_tape.cpp"
+
+SIMDJSON_POP_DISABLE_WARNINGS
