d83042248930b6faf18293004b497892e3425af8
Daniel Lemire <lemire@gmail.com>
2020-06-12 17:57:45 -0400

    Put back the amalgamation files and add tests (#929)
    Co-authored-by: John Keiser <john@johnkeiser.com>
    Co-authored-by: Daniel Lemire <lemire@gmai.com>

---
+++ singleheader/amalgamate_demo.cpp
@@ -0,0 +1,42 @@
+/* auto-generated on Fri 12 Jun 2020 13:09:36 EDT. Do not edit! */
+
+#include <iostream>
+#include "simdjson.h"
+#include "simdjson.cpp"
+int main(int argc, char *argv[]) {
+  if(argc < 2) {
+    std::cerr << "Please specify at least one file name. " << std::endl;
+  }
+  const char * filename = argv[1];
+  simdjson::dom::parser parser;
+  simdjson::error_code error;
+  UNUSED simdjson::dom::element elem;
+  parser.load(filename).tie(elem, error); // do the parsing
+  if (error) {
+    std::cout << "parse failed" << std::endl;
+    std::cout << "error code: " << error << std::endl;
+    std::cout << error << std::endl;
+    return EXIT_FAILURE;
+  } else {
+    std::cout << "parse valid" << std::endl;
+  }
+  if(argc == 2) {
+    return EXIT_SUCCESS;
+  }
+
+  // parse_many
+  const char * filename2 = argv[2];
+  for (auto result : parser.load_many(filename2)) {
+    error = result.error();
+  }
+  if (error) {
+    std::cout << "parse_many failed" << std::endl;
+    std::cout << "error code: " << error << std::endl;
+    std::cout << error << std::endl;
+    return EXIT_FAILURE;
+  } else {
+    std::cout << "parse_many valid" << std::endl;
+  }
+  return EXIT_SUCCESS;
+}
+
