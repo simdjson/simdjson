cbbdbb1777fe3e0d3bc87e32334ae72335527275
Daniel Lemire <lemire@gmail.com>
2020-06-29 18:17:56 -0400

    A few Keiser fixes

--- tests/numberparsingcheck.cpp
+++ tests/numberparsingcheck.cpp
@@ -89,7 +89,7 @@ void found_integer(int64_t result, const uint8_t *buf) {
   if ((endptr == (const char *)buf) || (expected != result)) {
 #if (!(__MINGW32__) && !(__MINGW64__))
     fprintf(stderr, "Error: parsed %" PRId64 " out of %.32s, ", result, buf);
-#else 
+#else // mingw is busted since we include #include <inttypes.h>
     fprintf(stderr, "Error: parsed %lld out of %.32s, ", (long long)result, buf);
 #endif
     fprintf(stderr, " while parsing %s \n", fullpath);
@@ -104,7 +104,7 @@ void found_unsigned_integer(uint64_t result, const uint8_t *buf) {
   if ((endptr == (const char *)buf) || (expected != result)) {
 #if (!(__MINGW32__) && !(__MINGW64__))
     fprintf(stderr, "Error: parsed %" PRIu64 " out of %.32s, ", result, buf);
-#else
+#else // mingw is busted since we include #include <inttypes.h>
     fprintf(stderr, "Error: parsed %llu out of %.32s, ", (unsigned long long)result, buf);
 #endif
     fprintf(stderr, " while parsing %s \n", fullpath);
