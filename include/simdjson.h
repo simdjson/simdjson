164fcb49d9cb3647e263a36d97eae72de0450368
Pavel P <pavlov.pavel@gmail.com>
2020-08-03 19:48:41 +0600

    + reformat sample code

--- include/simdjson.h
+++ include/simdjson.h
@@ -5,33 +5,33 @@
  * @mainpage
  *
  * Check the [README.md](https://github.com/lemire/simdjson/blob/master/README.md#simdjson--parsing-gigabytes-of-json-per-second).
+ * 
+ * Sample code. See https://github.com/simdjson/simdjson/blob/master/doc/basics.md for more examples.
 
-Sample code. See https://github.com/simdjson/simdjson/blob/master/doc/basics.md for more examples
+    #include "simdjson.h"
 
-#include "simdjson.h"
+    int main(void) {
+      // load from `twitter.json` file:
+      simdjson::dom::parser parser;
+      simdjson::dom::element tweets = parser.load("twitter.json");
+      std::cout << tweets["search_metadata"]["count"] << " results." << std::endl;
 
-int main(void) {
-  // load from `twitter.json` file:
-  simdjson::dom::parser parser;
-  simdjson::dom::element tweets = parser.load("twitter.json");
-  std::cout << tweets["search_metadata"]["count"] << " results." << std::endl;
-
-  // Parse and iterate through an array of objects
-  auto abstract_json = R"( [
-    {  "12345" : {"a":12.34, "b":56.78, "c": 9998877}   },
-    {  "12545" : {"a":11.44, "b":12.78, "c": 11111111}  }
-    ] )"_padded;
+      // Parse and iterate through an array of objects
+      auto abstract_json = R"( [
+        {  "12345" : {"a":12.34, "b":56.78, "c": 9998877}   },
+        {  "12545" : {"a":11.44, "b":12.78, "c": 11111111}  }
+        ] )"_padded;
   
-  for (simdjson::dom::object obj : parser.parse(abstract_json)) {
-    for(const auto& key_value : obj) {
-      cout << "key: " << key_value.key << " : ";
-      simdjson::dom::object innerobj = key_value.value;
-      cout << "a: " << double(innerobj["a"]) << ", ";
-      cout << "b: " << double(innerobj["b"]) << ", ";
-      cout << "c: " << int64_t(innerobj["c"]) << endl;
+      for (simdjson::dom::object obj : parser.parse(abstract_json)) {
+        for(const auto& key_value : obj) {
+          cout << "key: " << key_value.key << " : ";
+          simdjson::dom::object innerobj = key_value.value;
+          cout << "a: " << double(innerobj["a"]) << ", ";
+          cout << "b: " << double(innerobj["b"]) << ", ";
+          cout << "c: " << int64_t(innerobj["c"]) << endl;
+        }
+      }
     }
-  }
-}
  */
 
 #include "simdjson/compiler_check.h"
