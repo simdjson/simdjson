0c7ce3bb8fc5adebc9e8578c687e35e724f0cf42
John Keiser <john@johnkeiser.com>
2020-08-08 19:24:09 -0700

    Add DOM tweet reader for comparison

--- benchmark/bench_sax.cpp
+++ benchmark/bench_sax.cpp
@@ -238,8 +238,63 @@ using namespace benchmark;
 using namespace std;
 
 const char *TWITTER_JSON = SIMDJSON_BENCHMARK_DATA_DIR "twitter.json";
+const int REPETITIONS = 20;
 
-static void sax_tweets(State& state) {
+really_inline uint64_t nullable_int(dom::element element) {
+  if (element.is_null()) { return 0; }
+  return element;
+}
+really_inline void read_dom_tweets(dom::parser &parser, padded_string &json, std::vector<twitter::tweet> &tweets) {
+  for (dom::element tweet : parser.parse(json)["statuses"]) {
+    auto user = tweet["user"];
+    tweets.push_back(
+      {
+        tweet["id"],
+        tweet["text"],
+        tweet["created_at"],
+        nullable_int(tweet["in_reply_to_status_id"]),
+        nullable_int(tweet["retweet_count"]),
+        nullable_int(tweet["favorite_count"]),
+        { user["id"], user["screen_name"] }
+      }
+    );
+  }
+}
+
+static void dom_tweets(State &state) {
+  // Load twitter.json to a buffer
+  padded_string json;
+  if (auto error = padded_string::load(TWITTER_JSON).get(json)) { cerr << error << endl; return; }
+
+  // Allocate
+  dom::parser parser;
+  if (auto error = parser.allocate(json.size())) { cerr << error << endl; return; };
+
+  // Warm the vector
+  std::vector<twitter::tweet> tweets;
+  read_dom_tweets(parser, json, tweets);
+
+  // Read tweets
+  size_t bytes = 0;
+  size_t num_tweets = 0;
+  for (UNUSED auto _ : state) {
+    tweets.clear();
+    read_dom_tweets(parser, json, tweets);
+    bytes += json.size();
+    num_tweets += tweets.size();
+  }
+  // Gigabyte: https://en.wikipedia.org/wiki/Gigabyte
+  state.counters["Gigabytes"] = benchmark::Counter(
+	        double(bytes), benchmark::Counter::kIsRate,
+	        benchmark::Counter::OneK::kIs1000); // For GiB : kIs1024
+  state.counters["docs"] = Counter(double(state.iterations()), benchmark::Counter::kIsRate);
+  state.counters["tweets"] = Counter(double(num_tweets), benchmark::Counter::kIsRate);
+}
+BENCHMARK(dom_tweets)->Repetitions(REPETITIONS)->ComputeStatistics("max", [](const std::vector<double>& v) -> double {
+    return *(std::max_element(std::begin(v), std::end(v)));
+  })->DisplayAggregatesOnly(true);
+
+static void sax_tweets(State &state) {
   // Load twitter.json to a buffer
   padded_string json;
   if (auto error = padded_string::load(TWITTER_JSON).get(json)) { cerr << error << endl; return; }
@@ -248,7 +303,10 @@ static void sax_tweets(State& state) {
   twitter::sax_tweet_reader reader;
   if (auto error = reader.set_capacity(json.size())) { cerr << error << endl; return; }
 
-  // Make the tweet_reader
+  // Warm the vector
+  if (auto error = reader.read_tweets(json)) { throw error; }
+
+  // Read tweets
   size_t bytes = 0;
   size_t tweets = 0;
   for (UNUSED auto _ : state) {
@@ -263,7 +321,7 @@ static void sax_tweets(State& state) {
   state.counters["docs"] = Counter(double(state.iterations()), benchmark::Counter::kIsRate);
   state.counters["tweets"] = Counter(double(tweets), benchmark::Counter::kIsRate);
 }
-BENCHMARK(sax_tweets)->Repetitions(10)->ComputeStatistics("max", [](const std::vector<double>& v) -> double {
+BENCHMARK(sax_tweets)->Repetitions(REPETITIONS)->ComputeStatistics("max", [](const std::vector<double>& v) -> double {
     return *(std::max_element(std::begin(v), std::end(v)));
   })->DisplayAggregatesOnly(true);
 
