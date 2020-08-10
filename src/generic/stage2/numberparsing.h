9e93509a5677882ba02c201b6fc9cd6d38858831
Daniel Lemire <lemire@gmail.com>
2020-08-10 18:10:11 -0400

    Fix number parsing (too lenient). (#1107)
    * Fix number parsing (too lenient).
    
    * Minor tweak.
    
    * These are Booleans.
    
    * Tweaking test config

--- src/generic/stage2/numberparsing.h
+++ src/generic/stage2/numberparsing.h
@@ -460,7 +460,8 @@ really_inline bool parse_number(const uint8_t *const src, W &writer) {
     if (!parse_exponent(src, p, exponent)) { return false; }
   }
   if (is_float) {
-    return write_float(src, negative, i, start_digits, digit_count, exponent, writer);
+    const bool clean_end = is_structural_or_whitespace(*p);
+    return write_float(src, negative, i, start_digits, digit_count, exponent, writer) && clean_end;
   }
 
   // The longest negative 64-bit number is 19 digits.
