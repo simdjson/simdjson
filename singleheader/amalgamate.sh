e78394c611487871a6b16bce3e650cde5676717d
Daniel Lemire <lemire@gmail.com>
2020-08-10 18:36:13 -0400

    Please don't use GNU-specific flags.

--- singleheader/amalgamate.sh
+++ singleheader/amalgamate.sh
@@ -127,8 +127,8 @@ echo "/* auto-generated on ${timestamp}. Do not edit! */" > ${AMAL_C}
     done
 } >> ${AMAL_C}
 
-cp -u "${SCRIPTPATH}/amalgamate_demo.cpp" "${DEMOCPP}" 2>/dev/null
-cp -u "${SCRIPTPATH}/README.md" "${README}" 2>/dev/null
+cp -f "${SCRIPTPATH}/amalgamate_demo.cpp" "${DEMOCPP}"
+cp -f "${SCRIPTPATH}/README.md" "${README}"
 
 echo "Done with all files generation."
 
