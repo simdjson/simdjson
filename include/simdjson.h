3577c87c880bc55eef8ac811f059c72fea28f14e
John Keiser <john@johnkeiser.com>
2020-09-08 16:04:18 -0700

    Fix amalgamation with generic/ files in include/

--- include/simdjson.h
+++ include/simdjson.h
@@ -72,6 +72,15 @@ SIMDJSON_DISABLE_UNDESIRED_WARNINGS
 #include "simdjson/internal/tape_ref-inl.h"
 #include "simdjson/dom/serialization-inl.h"
 
+// Implementation-internal files (must be included before the implementations themselves, to keep
+// amalgamation working--otherwise, the first time a file is included, it might be put inside the
+// #ifdef SIMDJSON_IMPLEMENTATION_ARM64/FALLBACK/etc., which means the other implementations can't
+// compile unless that implementation is turned on).
+#include "simdjson/internal/isadetection.h"
+#include "simdjson/internal/jsoncharutils_tables.h"
+#include "simdjson/internal/numberparsing_tables.h"
+#include "simdjson/internal/simdprune_tables.h"
+
 // Implementations
 #include "simdjson/arm64.h"
 #include "simdjson/fallback.h"
