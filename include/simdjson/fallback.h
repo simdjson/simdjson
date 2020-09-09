b48c690e4f3569c375df93ee2cb74f1b4e2f3b88
John Keiser <john@johnkeiser.com>
2020-09-09 07:05:59 -0700

    Add ondemand to arm64/fallback/westmere

--- include/simdjson/fallback.h
+++ include/simdjson/fallback.h
@@ -8,12 +8,21 @@
 #include "simdjson/fallback/implementation.h"
 
 #include "simdjson/fallback/begin.h"
+
+// Declarations
 #include "simdjson/generic/dom_parser_implementation.h"
 #include "simdjson/fallback/bitmanipulation.h"
 #include "simdjson/generic/jsoncharutils.h"
 #include "simdjson/generic/atomparsing.h"
 #include "simdjson/fallback/stringparsing.h"
 #include "simdjson/fallback/numberparsing.h"
+#include "simdjson/generic/implementation_simdjson_result_base.h"
+#include "simdjson/generic/ondemand.h"
+
+// Inline definitions
+#include "simdjson/generic/implementation_simdjson_result_base-inl.h"
+#include "simdjson/generic/ondemand-inl.h"
+
 #include "simdjson/fallback/end.h"
 
 #endif // SIMDJSON_IMPLEMENTATION_FALLBACK
