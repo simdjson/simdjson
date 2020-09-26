d696a3b1ae45ef9109686ce75bbbae213522f33f
Paul Dreik <github@pauldreik.se>
2020-09-26 11:54:26 +0200

    updated ossfuzz.sh - disable more stuff, comment on new fuzzer

--- fuzz/ossfuzz.sh
+++ fuzz/ossfuzz.sh
@@ -7,9 +7,7 @@
 # invoke it from the git root.
 
 # make sure to exit on problems
-set -e
-set -u
-set -x
+set -eux
 
 for prog in zip cmake ninja; do
     if ! which $prog >/dev/null; then
@@ -32,13 +30,17 @@ cmake .. \
 -DENABLE_FUZZING=On \
 -DSIMDJSON_COMPETITION=Off \
 -DSIMDJSON_FUZZ_LINKMAIN=Off \
+-DSIMDJSON_GIT=Off \
+-DSIMDJSON_GOOGLE_BENCHMARKS=Off \
 -DSIMDJSON_FUZZ_LDFLAGS=$LIB_FUZZING_ENGINE
 
 cmake --build . --target all_fuzzers
 
 cp fuzz/fuzz_* $OUT
 
-# all corpora are equal, they all take json as input
+# all fuzzers but one (the tiny target for utf8 validation) takes json
+# as input, therefore use the same corpus of json files for all.
 for f in $(ls $OUT/fuzz* |grep -v '.zip$') ; do
    cp ../corpus.zip $OUT/$(basename $f).zip
 done
+
