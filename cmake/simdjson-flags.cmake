55a339c3c600f11bab6ae1533c7163d3fd9c4c06
friendlyanon <friendlyanon@users.noreply.github.com>
2020-10-24 14:20:05 +0000

    Move flag options to top level lists

--- cmake/simdjson-flags.cmake
+++ cmake/simdjson-flags.cmake
@@ -30,37 +30,9 @@ target_link_libraries(simdjson-internal-flags INTERFACE simdjson-flags)
 #
 # Other optional flags
 #
-option(SIMDJSON_ONDEMAND_SAFETY_RAILS "Validate ondemand user code at runtime to ensure it is being used correctly. Defaults to ON for debug builds, OFF for release builds." $<IF:$<CONFIG:DEBUG>,ON,OFF>)
-if(SIMDJSON_ONDEMAND_SAFETY_RAILS)
-  message(STATUS "Ondemand safety rails enabled. Ondemand user code will be checked at runtime. This will be slower than normal!")
-  target_compile_definitions(simdjson-flags INTERFACE SIMDJSON_ONDEMAND_SAFETY_RAILS)
-endif(SIMDJSON_ONDEMAND_SAFETY_RAILS)
 
-option(SIMDJSON_BASH "Allow usage of bash within CMake" ON)
 
-option(SIMDJSON_GIT "Allow usage of git within CMake" ON)
 
-option(SIMDJSON_EXCEPTIONS "Enable simdjson's exception-throwing interface" ON)
-if(NOT SIMDJSON_EXCEPTIONS)
-  message(STATUS "simdjson exception interface turned off. Code that does not check error codes will not compile.")
-  target_compile_definitions(simdjson-flags INTERFACE SIMDJSON_EXCEPTIONS=0)
-endif()
-
-option(SIMDJSON_ENABLE_THREADS "Link with thread support" ON)
-if(SIMDJSON_ENABLE_THREADS)
-  set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
-  set(THREADS_PREFER_PTHREAD_FLAG TRUE)
-  find_package(Threads REQUIRED)
-  target_link_libraries(simdjson-flags INTERFACE Threads::Threads)
-  target_link_libraries(simdjson-flags INTERFACE ${CMAKE_THREAD_LIBS_INIT})
-  target_compile_options(simdjson-flags INTERFACE ${CMAKE_THREAD_LIBS_INIT})
-  target_compile_definitions(simdjson-flags INTERFACE SIMDJSON_THREADS_ENABLED=1) # This will be set in the code automatically.
-endif()
-
-option(SIMDJSON_VERBOSE_LOGGING, "Enable verbose logging for internal simdjson library development." OFF)
-if (SIMDJSON_VERBOSE_LOGGING)
-  target_compile_definitions(simdjson-flags INTERFACE SIMDJSON_VERBOSE_LOGGING=1)
-endif()
 
 if(SIMDJSON_USE_LIBCPP)
   target_link_libraries(simdjson-flags INTERFACE -stdlib=libc++ -lc++abi)
