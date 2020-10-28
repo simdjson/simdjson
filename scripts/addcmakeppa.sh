40538d9910567a7f122ecc27c28f91e7b542d012
friendlyanon <friendlyanon@users.noreply.github.com>
2020-10-28 16:10:56 +0000

    Remove sudo calls in the CMake PPA script
    This script is intended to be run in Docker images, where the default
    user is already root.

--- scripts/addcmakeppa.sh
+++ scripts/addcmakeppa.sh
@@ -17,12 +17,12 @@ trap exit ERR
 
 # Commands taken from https://apt.kitware.com/
 
-sudo apt-get update -qq
-sudo apt-get install -y apt-transport-https ca-certificates gnupg software-properties-common wget
+apt-get update -qq
+apt-get install -y apt-transport-https ca-certificates gnupg software-properties-common wget
 
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
 
-sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $1 main"
-sudo apt-get update -qq
+apt-add-repository "deb https://apt.kitware.com/ubuntu/ $1 main"
+apt-get update -qq
 
 echo "CMake repository added successfully"
