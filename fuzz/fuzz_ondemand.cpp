726f59a5365de6e56981b1f7ae434b766cb16e57
Paul Dreik <github@pauldreik.se>
2020-10-31 19:35:04 +0100

    fuzz more of the ondemand api

--- fuzz/fuzz_ondemand.cpp
+++ fuzz/fuzz_ondemand.cpp
@@ -6,7 +6,7 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   FuzzData fd(Data, Size);
-  const int action = fd.getInt<0, 31>();
+  const int action = fd.getInt<0, 12>();
 
   // split the remainder of the document into strings
   auto strings = fd.splitIntoStrings();
@@ -47,6 +47,20 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
       } break;
       case 7: {
         simdjson_unused auto x = item.get_raw_json_string();
+      } break;
+      case 8: {
+        simdjson_unused auto x = item.is_null();
+      } break;
+      case 9: {
+        simdjson_unused auto x = item.begin();
+      } break;
+      case 10: {
+        simdjson_unused auto x = item.end();
+      } break;
+      case 11: {
+        for (auto e : item) {
+          simdjson_unused auto x = e.is_null();
+        }
       } break;
       default:;
       }
