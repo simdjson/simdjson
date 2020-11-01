3ad620ae412e9721aa8bf54e39d3430e3458f139
Paul Dreik <github@pauldreik.se>
2020-11-01 20:53:39 +0100

    initial try at python implementation of amalgamate.py

---
+++ singleheader/amalgamate.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python3
+
+import sys
+import os.path
+import subprocess
+from pathlib import Path
+import re
+
+SCRIPTPATH=os.path.dirname(os.path.abspath(sys.argv[0]))
+PROJECTPATH=os.path.dirname(SCRIPTPATH)
+
+print(f"SCRIPTPATH={SCRIPTPATH} PROJECTPATH={PROJECTPATH}")
+
+
+print("We are about to amalgamate all simdjson files into one source file.")
+print("See https://www.sqlite.org/amalgamation.html and https://en.wikipedia.org/wiki/Single_Compilation_Unit for rationale.")
+
+AMALGAMATE_SOURCE_PATH=os.path.join(PROJECTPATH,"src")
+AMALGAMATE_INCLUDE_PATH=os.path.join(PROJECTPATH,"include")
+AMALGAMATE_OUTPUT_PATH=os.path.join(SCRIPTPATH)
+
+# this list excludes the "src/generic headers"
+ALLCFILES=["simdjson.cpp"]
+
+# order matters
+ALLCHEADERS=["simdjson.h"]
+
+found_includes=[]
+
+
+def doinclude(fid,file,line):
+  p=Path(AMALGAMATE_INCLUDE_PATH)/file
+  pi=Path(AMALGAMATE_SOURCE_PATH)/file
+
+  if p.exists():
+    # generic includes are included multiple times
+    if re.match('.*generic/.*.h',file):
+      dofile(fid,AMALGAMATE_INCLUDE_PATH,file)
+    # begin/end_implementation are also included multiple times
+    elif re.match('.*/begin.h',file):
+      dofile(fid,AMALGAMATE_INCLUDE_PATH,file)
+    elif re.match('.*/end.h',file):
+      dofile(fid,AMALGAMATE_INCLUDE_PATH,file)
+    elif file not in found_includes:
+      found_includes.append(file)
+      dofile(fid,AMALGAMATE_INCLUDE_PATH,file)
+  elif pi.exists():
+    # generic includes are included multiple times
+    if re.match('.*generic/.*h',file):
+      dofile(fid,AMALGAMATE_SOURCE_PATH,file)
+    elif file not in found_includes:
+      found_includes.append(file)
+      dofile(fid,AMALGAMATE_SOURCE_PATH,file)
+    else:
+      print(f"//doinclude: {file} already included: {line}")
+  else:
+    # If we don't recognize it, just emit the #include
+    print(line,file=fid)
+
+def dofile(fid,prepath,filename):
+  print(f"// dofile: invoked with prepath={prepath}, filename={filename}",file=fid)
+  file=Path(prepath)/filename
+  RELFILE=file.relative_to(PROJECTPATH)
+  # Last lines are always ignored. Files should end by an empty lines.
+  print(f"/* begin file {RELFILE} */",file=fid)
+  fid2=open(file,'r')
+  for line in fid2:
+    line=line.rstrip('\n');
+    s=re.search('^#include "(.*)"',line)
+    if s:
+      includedfile=s.group(1)
+      # include all from simdjson.cpp except simdjson.h
+      if includedfile == "simdjson.h" and filename == "simdjson.cpp":
+         print(line,file=fid)
+         continue
+
+      if includedfile.startswith('../'):
+        includedfile=includedfile[2:]
+      # we explicitly include simdjson headers, one time each (unless they are generic, in which case multiple times is fine)
+      doinclude(fid,includedfile,line)
+    else:
+      # Otherwise we simply copy the line
+      print(line,file=fid)
+  print(f"/* end file {RELFILE} */",file=fid)
+
+
+# Get the generation date from git, so the output is reproducible.
+# The %ci specifier gives the unambiguous ISO 8601 format, and
+# does not change with locale and timezone at time of generation.
+# Forcing it to be UTC is difficult, because it needs to be portable
+# between gnu date and busybox date.
+timestamp = subprocess.run(['git','show','-s','--format=%ci','HEAD'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+print(f"timestamp is {timestamp}")
+
+Path(AMALGAMATE_OUTPUT_PATH).mkdir(parents=True, exist_ok=True)
+
+AMAL_H=os.path.join(AMALGAMATE_OUTPUT_PATH,"simdjson.h")
+AMAL_C=os.path.join(AMALGAMATE_OUTPUT_PATH,"simdjson.cpp")
+DEMOCPP=os.path.join(AMALGAMATE_OUTPUT_PATH,"amalgamate_demo.cpp")
+README=os.path.join(AMALGAMATE_OUTPUT_PATH,"README.md")
+
+print(f"Creating {AMAL_H}")
+amal_h=open(AMAL_H,'w')
+print(f"/* auto-generated on {timestamp}. Do not edit! */",file=amal_h);
+for h in ALLCHEADERS:
+  doinclude(amal_h,h,f"ERROR {h} not found")
+
+amal_h.close()
+
+print(f"Creating {AMAL_C}")
+amal_c=open(AMAL_C,'w')
+print(f"/* auto-generated on {timestamp}. Do not edit! */",file=amal_c);
+for c in ALLCFILES:
+  doinclude(amal_c,c,f"ERROR {c} not found")
+
+amal_c.close()
+
