27ab9b93977389d9b13d582e525bf3cce9b64bbc
Paul Dreik <github@pauldreik.se>
2020-11-07 14:44:52 +0100

    add ndjson parser

---
+++ fuzz/fuzz_ndjson.cpp
@@ -0,0 +1,27 @@
+#include "simdjson.h"
+#include <cstddef>
+#include <cstdint>
+#include <string>
+
+#include "FuzzUtils.h"
+#include "NullBuffer.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  FuzzData fd(Data, Size);
+  const auto batch_size=static_cast<size_t>(fd.getInt<0,1000>());
+  const auto json=simdjson::padded_string{fd.remainder_as_stringview()};
+  simdjson::dom::parser parser;
+#if SIMDJSON_EXCEPTIONS
+  try {
+#endif
+  simdjson::dom::document_stream docs = parser.parse_many(json,batch_size);
+  size_t bool_count=0;
+  for (simdjson::dom::element doc : docs) {
+    bool_count+=doc.is_bool();
+  }
+#if SIMDJSON_EXCEPTIONS
+  } catch(...) {
+  }
+#endif
+  return 0;
+}
