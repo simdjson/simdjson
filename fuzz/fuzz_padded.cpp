2a4236b8e88f16202affc7c9e3d047937f51b00f
Paul Dreik <github@pauldreik.se>
2020-11-18 20:09:01 +0100

    fuzz also assignment

--- fuzz/fuzz_padded.cpp
+++ fuzz/fuzz_padded.cpp
@@ -7,7 +7,22 @@
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   FuzzData fd(Data,Size);
 
-  const auto s=fd.get<size_t>();
-  simdjson_unused simdjson::padded_string p(s);
+  const int action=fd.getInt<0,10>();
+
+  const auto s1=fd.get<size_t>();
+  const auto s2=fd.get<size_t>();
+  switch(action) {
+  case 0: {
+    simdjson_unused simdjson::padded_string p(s1);
+  }break;
+  case 1: {
+    // operator== with temp value
+    simdjson_unused simdjson::padded_string p1(s1);
+    simdjson_unused simdjson::padded_string p2(s2);
+    p1=std::move(p2);
+  }break;
+  default:
+    ;
+  }
   return  0;
 }
