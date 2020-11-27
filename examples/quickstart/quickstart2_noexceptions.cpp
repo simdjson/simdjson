3fa40b8dc207ae9c4be5ba177fdc84a68b414dad
Daniel Lemire <lemire@gmail.com>
2020-11-27 17:40:29 -0500

    Adding an example corresponding to issue 1316 (documentation enhancement) (#1317)
    * Adding an example.
    
    * Updated other doc file.
    
    * Trying to take into account @jkeiser's comments.
    
    * Some people prefer empty final lines.

---
+++ examples/quickstart/quickstart2_noexceptions.cpp
@@ -0,0 +1,13 @@
+#include "simdjson.h"
+
+int main(void) {
+  simdjson::dom::parser parser;
+  simdjson::dom::element tweets;
+  auto error = parser.load("twitter.json").get(tweets);
+  if(error) { std::cerr << error << std::endl; return EXIT_FAILURE; }
+  uint64_t identifier;
+  error = tweets["statuses"].at(0)["id"].get(identifier);
+  if(error) { std::cerr << error << std::endl; return EXIT_FAILURE; }
+  std::cout << identifier << std::endl;
+  return EXIT_SUCCESS;
+}
