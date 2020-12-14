15297c12d1d970ef30bcc62b44d3207a61f9a531
Daniel Lemire <lemire@gmail.com>
2020-12-13 20:01:10 -0500

    Updating according to John's comments.

--- include/simdjson/ppc64.h
+++ include/simdjson/ppc64.h
@@ -9,8 +9,8 @@
 
 #ifndef SIMDJSON_IMPLEMENTATION_PPC64
 #define SIMDJSON_IMPLEMENTATION_PPC64 (SIMDJSON_IS_PPC64)
-#define SIMDJSON_CAN_ALWAYS_RUN_PPC64 (SIMDJSON_IS_PPC64)
 #endif
+#define SIMDJSON_CAN_ALWAYS_RUN_PPC64 SIMDJSON_IMPLEMENTATION_PPC64 && SIMDJSON_IS_PPC64
 
 #include "simdjson/internal/isadetection.h"
 #include "simdjson/internal/jsoncharutils_tables.h"
