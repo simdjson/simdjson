092ef0af36f424bb185ec0936e77e3358552cded
Daniel Lemire <lemire@gmail.com>
2020-12-18 17:18:50 -0500

    Adding puzzler scenario.

--- tests/unicode_tests.cpp
+++ tests/unicode_tests.cpp
@@ -242,7 +242,30 @@ void test() {
   }
   printf("tests ok.\n");
 }
+
+// This is an attempt at reproducing an issue with the utf8 fuzzer
+void puzzler() {
+  std::cout << "running puzzler... " << std::endl;
+  const char* bad64 = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x1c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
+  size_t length = 64;
+  std::cout << "Input: \"";
+  for(size_t j = 0; j < length; j++) {
+    std::cout << "\\x" << std::hex << std::setw(2) << std::setfill('0') << uint32_t(bad64[j]);
+  }
+  std::cout << "\"" << std::endl;
+  bool is_ok{true};
+  for(const auto& e: simdjson::available_implementations) {
+      if(!e->supported_by_runtime_system()) { continue; }
+      const bool current = e->validate_utf8(bad64, length);
+      std::cout << e->name() << " returns " << current << std::endl;
+      if(current) { is_ok = false; }
+  }
+  if(!is_ok) { abort(); }
+  std::cout << "Ok!" << std::endl;
+}
+
 int main() {
+  puzzler();
   brute_force_tests();
   test();
   return EXIT_SUCCESS;
