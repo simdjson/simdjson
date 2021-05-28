9bbf17c04e99e2be5e8b0bfec5f9f49b1a28a873
Nicolas Boyer <nicolasjiaxin@gmail.com>
2021-05-28 19:23:14 +0000

    Remove trailing whitespaces

--- benchmark/kostya/rapidjson_sax.h
+++ benchmark/kostya/rapidjson_sax.h
@@ -15,10 +15,10 @@ struct rapidjson_sax {
         size_t k{0};
         double buffer[3];
         std::vector<point>* result;
-        
+
         Handler(std::vector<point> &r) { result=&r; }
 
-        bool Key(const char* key, SizeType length, bool copy) { 
+        bool Key(const char* key, SizeType length, bool copy)
             switch(key[0]) {
             case 'x':
                 k = 0;
@@ -28,17 +28,17 @@ struct rapidjson_sax {
                 break;
             case 'z':
                 k = 2;
-                break;  
+                break;
             }
-            return true; 
+            return true;
         }
-        bool Double(double d) { 
+        bool Double(double d) {
             buffer[k] = d;
             if (k==2) {
                 (*result).emplace_back(json_benchmark::point{buffer[0],buffer[1],buffer[2]});
                 k = 0;
             }
-            return true; 
+            return true;
         }
         bool Uint(unsigned i) {     // Need this event because coordinate value can be equal to 1
             return Double(i);
@@ -56,7 +56,7 @@ struct rapidjson_sax {
         bool StartArray() { return true; }
         bool EndArray(SizeType elementCount) { return true; }
     }; // handler
-    
+
     bool run(simdjson::padded_string &json, std::vector<point> &result) {
         Reader reader;
         Handler handler(result);
