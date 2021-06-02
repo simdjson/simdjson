8b5b639b18c76515159d81fdd011c52f88ead9d7
Nicolas Boyer <nicolasjiaxin@gmail.com>
2021-06-02 18:25:06 +0000

    Add rapidjson_sax.h .

---
+++ benchmark/find_tweet/rapidjson_sax.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#ifdef SIMDJSON_COMPETITION_RAPIDJSON
+
+#include "find_tweet.h"
+#include <string.h>
+
+namespace find_tweet {
+
+using namespace rapidjson;
+
+struct rapidjson_sax {
+  using StringType=std::string_view;
+
+struct Handler {
+        bool text_key = false;
+        bool found_id = false;
+        uint64_t find_id;
+        std::string_view &result;
+
+        Handler(std::string_view &r,uint64_t id): result(r), find_id(id) { }
+
+        bool Key(const char* key, SizeType length, bool copy) {
+            if (found_id) { // If have found id, find text key
+                if ((length == 4) && (memcmp(key,"text",4) == 0)) { text_key = true; }
+            }
+            return true;
+        }
+        bool Uint64(uint64_t i) {
+            if (i == find_id) {  // Looking for find_id
+                found_id = true;
+            }
+            return true;
+        }
+        bool String(const char* str, SizeType length, bool copy) {
+            if (found_id && text_key) {
+                result = {str,length};
+                return false;   // End parsing
+            }
+            return true;
+        }
+        // Irrelevant events
+        bool Null() { return true; }
+        bool Bool(bool b) { return true; }
+        bool Double(double d) { return true; }
+        bool Int(int i) { return true; }
+        bool Int64(int64_t i) { return true; }
+        bool Uint(unsigned i) { return true; }
+        bool RawNumber(const char* str, SizeType length, bool copy) { return true; }
+        bool StartObject() { return true; }
+        bool EndObject(SizeType memberCount) { return true; }
+        bool StartArray() { return true; }
+        bool EndArray(SizeType elementCount) { return true; }
+    }; // handler
+
+  bool run(simdjson::padded_string &json, uint64_t find_id, std::string_view &result) {
+        Reader reader;
+        Handler handler(result,find_id);
+        InsituStringStream ss(json.data());
+        reader.Parse<kParseInsituFlag | kParseValidateEncodingFlag | kParseFullPrecisionFlag>(ss,handler);
+        return true;
+    }
+}; // rapidjson_sax
+BENCHMARK_TEMPLATE(find_tweet, rapidjson_sax)->UseManualTime();
+} // namespace find_tweet
+
+#endif // SIMDJSON_COMPETITION_RAPIDJSON
