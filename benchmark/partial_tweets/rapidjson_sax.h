52f79213cd3be7744b165e3e63659b1d6fec86ef
Nicolas Boyer <nicolasjiaxin@gmail.com>
2021-06-03 16:17:18 +0000

    Change std::string_view assignment in rapidjson_sax.

--- benchmark/partial_tweets/rapidjson_sax.h
+++ benchmark/partial_tweets/rapidjson_sax.h
@@ -113,17 +113,17 @@ struct rapidjson_sax {
         }
         bool String(const char* str, SizeType length, bool copy) {
             if (values & key_date && !(values & found_date)) {   //  created_at
-                date = str;
+                date = {str,length};
                 values &= ~(key_date);
                 values |= (found_date);
             }
             else if (values & key_text && !(values & found_text)) {   // text
-                text = str;
+                text = {str,length};
                 values &= ~(key_text);
                 values |= (found_text);
             }
             else if (values & key_screenname && !(values & found_screenname)) {    // user.screen_name
-                screen_name = str;
+                screen_name = {str,length};
                 userobject_screen_name = false;
                 values &= ~(key_screenname);
                 values |= (found_screenname);
