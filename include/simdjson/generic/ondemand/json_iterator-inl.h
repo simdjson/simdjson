438c2a28ff2e65825582f535b5a1ade7f6b99b3e
Daniel Lemire <lemire@gmail.com>
2021-06-04 17:09:21 -0400

    Preparing patch release.

--- include/simdjson/generic/ondemand/json_iterator-inl.h
+++ include/simdjson/generic/ondemand/json_iterator-inl.h
@@ -6,6 +6,7 @@ simdjson_really_inline json_iterator::json_iterator(json_iterator &&other) noexc
   : token(std::forward<token_iterator>(other.token)),
     parser{other.parser},
     _string_buf_loc{other._string_buf_loc},
+    error{other.error},
     _depth{other._depth}
 {
   other.parser = nullptr;
@@ -14,6 +15,7 @@ simdjson_really_inline json_iterator &json_iterator::operator=(json_iterator &&o
   token = other.token;
   parser = other.parser;
   _string_buf_loc = other._string_buf_loc;
+  error = other.error;
   _depth = other._depth;
   other.parser = nullptr;
   return *this;
