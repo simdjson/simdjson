aacd6fac2a1c8246fd62a128be10ec0245b84af7
Daniel Lemire <lemire@gmail.com>
2021-06-21 17:23:54 -0400

    Added tests over invalid documents.

--- tests/ondemand/ondemand_json_pointer_tests.cpp
+++ tests/ondemand/ondemand_json_pointer_tests.cpp
@@ -123,9 +123,50 @@ namespace json_pointer_tests {
         if (measured != expected) { return false; }
         TEST_SUCCEED();
     }
+    bool run_broken_tests() {
+        TEST_START();
+        ondemand::parser parser;
+        ondemand::document doc;
+        ondemand::value v;
+        std::string_view val;
+
+        auto invalid_escape_key = R"( {"hello": [0,1,2,3], "te\est": "foo", "bool": true, "num":1234, "success":"yes"} )"_padded;
+        auto invalid_escape_value = R"( {"hello": [0,1,2,3], "test": "fo\eo", "bool": true, "num":1234, "success":"yes"} )"_padded;
+        auto invalid_escape_value_at_jp = R"( {"hello": [0,1,2,3], "test": "foo", "bool": true, "num":1234, "success":"y\es"} )"_padded;
+        auto unclosed_object = R"( {"test": "foo", "bool": true, "num":1234, "success":"yes" )"_padded;
+        auto missing_bracket_before = R"( {"hello": [0,1,2,3, "test": "foo", "bool": true, "num":1234, "success":"yes"} )"_padded;
+        auto missing_bracket_after = R"( {"test": "foo", "bool": true, "num":1234, "success":"yes", "hello":[0,1,2,3} )"_padded;
+
+        std::string json_pointer = "/success";
+        std::cout << "\t- invalid_escape_key" << std::endl;
+        ASSERT_SUCCESS(parser.iterate(invalid_escape_key).get(doc));
+        ASSERT_ERROR(doc.at_pointer(json_pointer).get(val), simdjson::SUCCESS);
+        std::cout << "\t- invalid_escape_value" << std::endl;
+        ASSERT_SUCCESS(parser.iterate(invalid_escape_value).get(doc));
+        ASSERT_ERROR(doc.at_pointer(json_pointer).get(val), simdjson::SUCCESS);
+        std::cout << "\t- invalid_escape_value_at_jp_nomat" << std::endl;
+        ASSERT_SUCCESS(parser.iterate(invalid_escape_value_at_jp).get(doc));
+        ASSERT_ERROR(doc.at_pointer(json_pointer).get(v), simdjson::SUCCESS);
+        std::cout << "\t- invalid_escape_value_at_jp" << std::endl;
+        ASSERT_SUCCESS(parser.iterate(invalid_escape_value_at_jp).get(doc));
+        ASSERT_ERROR(doc.at_pointer(json_pointer).get(val), simdjson::STRING_ERROR);
+        std::cout << "\t- unclosed_object" << std::endl;
+        ASSERT_SUCCESS(parser.iterate(unclosed_object).get(doc));
+        ASSERT_ERROR(doc.at_pointer(json_pointer).get(val), simdjson::TAPE_ERROR);
+        std::cout << "\t- missing_bracket_before" << std::endl;
+        ASSERT_SUCCESS(parser.iterate(missing_bracket_before).get(doc));
+        ASSERT_ERROR(doc.at_pointer(json_pointer).get(val), simdjson::TAPE_ERROR);
+        std::cout << "\t- missing_bracket_after" << std::endl;
+        ASSERT_SUCCESS(parser.iterate(missing_bracket_after).get(doc));
+        ASSERT_ERROR(doc.at_pointer(json_pointer).get(val), simdjson::SUCCESS);
+
+        TEST_SUCCEED();
+    }
+
 
     bool run() {
         return
+                run_broken_tests() &&
                 demo_test() &&
                 demo_relative_path() &&
                 run_success_test(TEST_RFC_JSON,"",R"({"foo":["bar","baz"],"":0,"a/b":1,"c%d":2,"e^f":3,"g|h":4,"i\\j":5,"k\"l":6," ":7,"m~n":8})") &&
