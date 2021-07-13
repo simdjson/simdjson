23374b32018d0cc467b72714dc179a849ee4f9de
Daniel Lemire <lemire@gmail.com>
2021-07-13 16:22:01 -0400

    Another attempt at producing problems with threads.

--- tests/dom/document_stream_tests.cpp
+++ tests/dom/document_stream_tests.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cctype>
 #include <unistd.h>
+#include <random>
 
 #include "simdjson.h"
 #include "test_macros.h"
@@ -823,8 +824,38 @@ namespace document_stream_tests {
     return true;
   }
 
+  bool baby_fuzzer() {
+    std::cout << "Running " << __func__ << std::endl;
+    std::mt19937 gen(637);
+    std::uniform_int_distribution<size_t> bs(0,1000);
+    std::uniform_int_distribution<size_t> len(0,10);
+    std::uniform_int_distribution<char> ascii(0,char(255));
+    for(size_t i = 0; i < 10000; i++) {
+      if((i%1000) == 0) { std::cout << "."; std::cout.flush(); }
+      size_t batch_size = bs(gen);
+      size_t l = len(gen);
+      std::unique_ptr<char[]> buffer(new char[l]);
+      for(size_t z = 0; z < l; z++) {
+        buffer.get()[z] = ascii(gen);
+      }
+      const auto json = simdjson::padded_string(buffer.get(), l);
+      simdjson::dom::parser parser;
+      simdjson::dom::document_stream docs;
+      if(parser.parse_many(json,batch_size).get(docs)) {
+        return false;
+      }
+      simdjson_unused size_t bool_count = 0;
+      for (auto doc : docs) {
+        bool_count += doc.is_bool();
+      }
+    }
+    std::cout << std::endl;
+    return true;
+  }
+
   bool run() {
-    return issue1649() &&
+    return baby_fuzzer() &&
+           issue1649() &&
            adversarial_single_document_array() &&
            adversarial_single_document() &&
            unquoted_key() &&
