ed512de0f953b4a0eb56426fcd2b6154046828a3
Daniel Lemire <lemire@gmail.com>
2021-07-26 12:46:49 -0400

    Adding test.

--- tests/ondemand/ondemand_document_stream_tests.cpp
+++ tests/ondemand/ondemand_document_stream_tests.cpp
@@ -349,6 +349,27 @@ namespace document_stream_tests {
     }
 
 
+    bool issue1668() {
+        TEST_START();
+        auto json = R"([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100])"_padded;
+
+        ondemand::parser odparser;
+        ondemand::document_stream odstream;
+        auto oderror = odparser.iterate_many(json.data(), json.length(), 50).get(odstream);
+
+        if (oderror) { std::cerr << "ondemand iterate_many error: " << oderror << std::endl; return false; }
+
+        for (auto &doc: odstream) {
+            ondemand::value val;
+            auto err = doc.at_pointer("/40").get(val);
+            if (err) {
+                std::cout << "ondemand: error accessing jsonpointer: " << err << std::endl;
+            } else {
+                std::cout << "ondemand: " << val << std::endl;
+            }
+        }
+        TEST_SUCCEED();
+    }
     bool document_stream_utf8_test() {
         TEST_START();
         fflush(NULL);
@@ -424,6 +445,7 @@ namespace document_stream_tests {
 
     bool run() {
         return
+            issue1668() &&
             simple_document_iteration() &&
             simple_document_iteration_multiple_batches() &&
             simple_document_iteration_with_parsing() &&
