c84da434ed0e6cc275dc3bd953347ed056c9b572
Daniel Lemire <lemire@gmail.com>
2021-10-07 11:19:34 -0400

    Tweaking.

--- tests/ondemand/ondemand_document_stream_tests.cpp
+++ tests/ondemand/ondemand_document_stream_tests.cpp
@@ -17,7 +17,7 @@ namespace document_stream_tests {
         // This example is not a good application of iterate_many since there is
         // a single JSON document.
         TEST_START();
-        std::string json = R"({
+        auto json = R"({
 	"t": 5649,
 	"ng": 5,
 	"lu": 4086,
@@ -133,7 +133,7 @@ namespace document_stream_tests {
 			]
 		}
 	]
-})";
+})"_padded;
 
         ondemand::parser parser;
         ondemand::document_stream stream;
@@ -157,10 +157,10 @@ namespace document_stream_tests {
 
     bool issue1683() {
         TEST_START();
-        std::string json = R"([1,2,3,4,5]
+        auto json = R"([1,2,3,4,5]
 [1,2,3,4,5]
 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
-[1,2,3,4,5])";
+[1,2,3,4,5])"_padded;
 
         ondemand::parser odparser;
         ondemand::document_stream odstream;
