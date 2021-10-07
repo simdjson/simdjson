3d66713a49e5d579e5349b9bf32bfd2122d582f7
Daniel Lemire <lemire@gmail.com>
2021-10-07 11:33:43 -0400

    More tweaking.

--- tests/ondemand/ondemand_document_stream_tests.cpp
+++ tests/ondemand/ondemand_document_stream_tests.cpp
@@ -13,6 +13,36 @@ namespace document_stream_tests {
         ASSERT_EQUAL(val, 5);
         return true;
     }
+    bool truncated_utf8() {
+        TEST_START();
+        // truncated UTF-8
+        auto json = "\"document1\" \xC3"_padded;
+
+        ondemand::parser parser;
+        ondemand::document_stream stream;
+        auto oderror = parser.iterate_many(json).get(stream);
+        if (oderror) {
+          std::cerr << "ondemand iterate_many error: " << oderror << std::endl;
+          return false;
+        }
+        int document_index = 0;
+        auto i = stream.begin();
+        for (; i != stream.end(); ++i) {
+            ondemand::document_reference doc;
+            auto err = (*i).get(doc);
+            document_index++;
+            std::cout << err << std::endl;
+            if((err == SUCCESS) && (document_index != 1)) {
+              std::cerr << "Only the first document should be valid." << std::endl;
+              return false;
+            }
+            if((err != SUCCESS) && (document_index != 2)) {
+              std::cerr << "ondemand iterate_many error: " << err << std::endl;
+              return false;
+            }
+        }
+        TEST_SUCCEED();
+    }
     bool issue1729() {
         // This example is not a good application of iterate_many since there is
         // a single JSON document.
@@ -157,10 +187,10 @@ namespace document_stream_tests {
 
     bool issue1683() {
         TEST_START();
-        auto json = R"([1,2,3,4,5]
+        std::string json = R"([1,2,3,4,5]
 [1,2,3,4,5]
 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
-[1,2,3,4,5])"_padded;
+[1,2,3,4,5])";
 
         ondemand::parser odparser;
         ondemand::document_stream odstream;
@@ -677,6 +707,7 @@ namespace document_stream_tests {
 
     bool run() {
         return
+            truncated_utf8() &&
             issue1729() &&
             fuzzaccess() &&
             issue1683() &&
