e94f0273101f4a7701c96365a71fcd774cf15f5e
Daniel Lemire <daniel@lemire.me>
2022-11-23 10:31:42 -0500

    Serialize 0 as 0.0 (#1921)
    * Serialize 0 as 0.0
    
    * Extending the '.0'.

--- src/to_chars.cpp
+++ src/to_chars.cpp
@@ -860,9 +860,9 @@ inline char *format_buffer(char *buf, int len, int decimal_exponent,
 
     std::memset(buf + k, '0', static_cast<size_t>(n) - static_cast<size_t>(k));
     // Make it look like a floating-point number (#362, #378)
-    // buf[n + 0] = '.';
-    // buf[n + 1] = '0';
-    return buf + (static_cast<size_t>(n));
+    buf[n + 0] = '.';
+    buf[n + 1] = '0';
+    return buf + (static_cast<size_t>(n)) + 2;
   }
 
   if (0 < n && n <= max_exp) {
@@ -925,10 +925,8 @@ char *to_chars(char *first, const char *last, double value) {
   {
     *first++ = '0';
     // Make it look like a floating-point number (#362, #378)
-    if(negative) {
-      *first++ = '.';
-      *first++ = '0';
-    }
+    *first++ = '.';
+    *first++ = '0';
     return first;
   }
   // Compute v = buffer * 10^decimal_exponent.
