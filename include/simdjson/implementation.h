8c89ae0cd9f50a018f57d7dda2f2138a97a05ccb
Daniel Lemire <daniel@lemire.me>
2023-01-13 09:17:05 -0500

    Removing extra ';'.

--- include/simdjson/implementation.h
+++ include/simdjson/implementation.h
@@ -87,7 +87,7 @@ public:
    *
    * @return a mask of all required `internal::instruction_set::` values.
    */
-  virtual uint32_t required_instruction_sets() const { return _required_instruction_sets; };
+  virtual uint32_t required_instruction_sets() const { return _required_instruction_sets; }
 
   /**
    * @private For internal implementation use
