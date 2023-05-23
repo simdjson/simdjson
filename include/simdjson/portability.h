df4e884cf31ab7776381325624320bcadd3c8c86
Daniel Lemire <dlemire@lemire.me>
2023-05-22 20:00:33 -0400

    Removing iso header.

--- include/simdjson/portability.h
+++ include/simdjson/portability.h
@@ -32,13 +32,6 @@
 #endif // __clang__
 #endif // _MSC_VER
 
-#if SIMDJSON_REGULAR_VISUAL_STUDIO
-// https://en.wikipedia.org/wiki/C_alternative_tokens
-// This header should have no effect, except maybe
-// under Visual Studio.
-#include <iso646.h>
-#endif
-
 #if defined(__x86_64__) || defined(_M_AMD64)
 #define SIMDJSON_IS_X86_64 1
 #elif defined(__aarch64__) || defined(_M_ARM64)
