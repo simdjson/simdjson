9ca6ce8aaa706a72e3c27fb5a709969eaa0f782a
Yong Xiang Ng <yxng@drwholdings.com>
2023-06-06 16:44:04 +0800

    Make tests work for exceptions

--- tests/ondemand/ondemand_iterate_many_csv.cpp
+++ tests/ondemand/ondemand_iterate_many_csv.cpp
@@ -8,13 +8,12 @@ using namespace simdjson;
 namespace iterate_many_csv_tests {
 using namespace std;
 
-#if SIMDJSON_EXCEPTIONS
-
 bool normal() {
   TEST_START();
   auto json = R"( 1, 2, 3, 4, "a", "b", "c", {"hello": "world"} , [1, 2, 3])"_padded;
   ondemand::parser parser;
-  ondemand::document_stream doc_stream = parser.iterate_many(json, json.size(), true);
+  ondemand::document_stream doc_stream;
+  ASSERT_SUCCESS(parser.iterate_many(json, json.size(), true).get(doc_stream));
 
   for (auto doc : doc_stream)
   {
@@ -28,7 +27,8 @@ bool small_batch_size() {
   TEST_START();
   auto json = R"( 1, 2, 3, 4, "a", "b", "c", {"hello": "world"} , [1, 2, 3])"_padded;
   ondemand::parser parser;
-  ondemand::document_stream doc_stream = parser.iterate_many(json, 32, true);
+  ondemand::document_stream doc_stream;
+  ASSERT_SUCCESS(parser.iterate_many(json, 32, true).get(doc_stream));
 
   for (auto doc : doc_stream)
   {
@@ -42,7 +42,8 @@ bool trailing_comma() {
   TEST_START();
   auto json = R"(1,)"_padded;
   ondemand::parser parser;
-  ondemand::document_stream doc_stream = parser.iterate_many(json, json.size(), true);
+  ondemand::document_stream doc_stream;
+  ASSERT_SUCCESS(parser.iterate_many(json, json.size(), true).get(doc_stream));
 
   for (auto doc : doc_stream)
   {
@@ -56,7 +57,8 @@ bool leading_comma() {
   TEST_START();
   auto json = R"(,1)"_padded;
   ondemand::parser parser;
-  ondemand::document_stream doc_stream = parser.iterate_many(json, json.size(), true);
+  ondemand::document_stream doc_stream;
+  ASSERT_SUCCESS(parser.iterate_many(json, json.size(), true).get(doc_stream));
 
   try {
     auto begin = doc_stream.begin();
@@ -74,7 +76,8 @@ bool check_parsed_values() {
 
   auto json = R"(  1  , "a" , [100, 1]  , {"hello"  :    "world"}  , )"_padded;
   ondemand::parser parser;
-  ondemand::document_stream doc_stream = parser.iterate_many(json, json.size(), true);
+  ondemand::document_stream doc_stream;
+  ASSERT_SUCCESS(parser.iterate_many(json, json.size(), true).get(doc_stream));
 
   auto begin = doc_stream.begin();
   auto end = doc_stream.end();
@@ -85,29 +88,45 @@ bool check_parsed_values() {
     switch (cnt)
     {
     case 0:
-      ASSERT_EQUAL(int(doc.get_int64()), 1);
+    {
+      int64_t actual;
+      ASSERT_SUCCESS(doc.get_int64().get(actual));
+      ASSERT_EQUAL(actual, 1);
       break;
+    }
     case 1:
-      ASSERT_EQUAL(std::string_view(doc.get_string()), "a");
+    {
+      std::string_view sv;
+      ASSERT_SUCCESS(doc.get_string().get(sv));
+      ASSERT_EQUAL(sv, "a");
       break;
+    }
     case 2:
     {
       std::vector<int64_t> expected{100, 1};
-      ondemand::array arr = doc.get_array();
-      ASSERT_EQUAL(int(arr.count_elements()), 2);
+      ondemand::array arr;
+      ASSERT_SUCCESS(doc.get_array().get(arr));
+      uint64_t element_count;
+      ASSERT_SUCCESS(arr.count_elements().get(element_count));
+      ASSERT_EQUAL(element_count, 2);
       int i = 0;
       for (auto a : arr)
       {
-        ASSERT_EQUAL(int64_t(a), expected[i++]);
+        int64_t actual;
+        ASSERT_SUCCESS(a.get(actual));
+        ASSERT_EQUAL(actual, expected[i++]);
       }
-    }
       break;
+    }
     case 3:
     {
-      ondemand::object obj = doc.get_object();
-      ASSERT_EQUAL(std::string_view(obj["hello"]), "world");
-    }
+      ondemand::object obj;
+      ASSERT_SUCCESS(doc.get_object().get(obj));
+      std::string_view sv;
+      obj.find_field("hello").get(sv);
+      ASSERT_EQUAL(sv, "world");
       break;
+    }
     default:
       TEST_FAIL("Too many cases")
     }
@@ -119,17 +138,12 @@ bool check_parsed_values() {
   TEST_SUCCEED();
 }
 
-#endif
-
 bool run() {
-  return
-#if SIMDJSON_EXCEPTIONS
-         normal() &&
+  return normal() &&
          small_batch_size() &&
          trailing_comma() &&
          leading_comma() &&
          check_parsed_values() &&
-#endif
          true;
 }
 
