6033525ef3ed312aedcc4662af16e16468a9e46d
halx99 <halx99@live.com>
2024-05-24 03:51:26 +0800

    Fix compile error on llvm-19

--- include/simdjson/dom/serialization.h
+++ include/simdjson/dom/serialization.h
@@ -57,15 +57,15 @@ public:
   simdjson_inline void one_char(char c);
 
   simdjson_inline void call_print_newline() {
-      this->print_newline();
+      static_cast<formatter*>(this)->print_newline();
   }
 
   simdjson_inline void call_print_indents(size_t depth) {
-      this->print_indents(depth);
+      static_cast<formatter*>(this)->print_indents(depth);
   }
 
   simdjson_inline void call_print_space() {
-      this->print_space();
+      static_cast<formatter*>(this)->print_space();
   }
 
 protected:
