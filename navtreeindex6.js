5663b1d3623eb825f8299801da8e8e6ad3ab64ff
lemire <lemire@users.noreply.github.com>
2024-05-23 21:12:31 +0000

    deploy: d2954ef68b653d7f2ba73901cb3be48a8057423d

--- navtreeindex6.js
+++ navtreeindex6.js
@@ -1,11 +1,5 @@
 var NAVTREEINDEX6 =
 {
-"value__iterator-inl_8h_source.html":[12,0,0,0,4,0,35],
-"value__iterator_8h_source.html":[12,0,0,0,4,0,36],
-"westmere_2base_8h_source.html":[12,0,0,0,12,0],
-"westmere_2begin_8h_source.html":[12,0,0,0,12,1],
-"westmere_2bitmanipulation_8h_source.html":[12,0,0,0,12,2],
-"westmere_2bitmask_8h_source.html":[12,0,0,0,12,3],
 "westmere_2end_8h_source.html":[12,0,0,0,12,4],
 "westmere_2implementation_8h_source.html":[12,0,0,0,12,5],
 "westmere_2intrinsics_8h_source.html":[12,0,0,0,12,6],
@@ -13,5 +7,11 @@ var NAVTREEINDEX6 =
 "westmere_2ondemand_8h_source.html":[12,0,0,0,12,8],
 "westmere_2simd_8h_source.html":[12,0,0,0,12,9],
 "westmere_2stringparsing__defs_8h_source.html":[12,0,0,0,12,10],
-"westmere_8h_source.html":[12,0,0,0,39]
+"westmere_8h_source.html":[12,0,0,0,39],
+"":[10,0,1],
+"":[10,0,0,9,0],
+"":[10,0,0,9,1],
+"":[10,0,0,9,2,0],
+"":[10,0,0,7],
+"":[10,0,0,9]
 };
