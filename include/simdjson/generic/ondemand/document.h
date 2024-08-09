6e61b7f6efb7e9941d3dc3f61bcd09c4239112bd
M. Bahoosh <moisrex@gmail.com>
2024-08-09 09:25:52 -1000

    Making `tag_invoke` to support `ondemand::document` as well + docs (#2228)
    * Making `tag_invoke` to support `ondemand::document` as well + docs
    
    * Fix typos and doc update by @lemire
    
    Co-authored-by: Daniel Lemire <daniel@lemire.me>
    
    * Better docs by @lemire
    
    Co-authored-by: Daniel Lemire <daniel@lemire.me>
    
    * Preserving the old, disallowing in the new
    
    I'm disabling `document::get() &&` if the user has provided a `tag_invoke`d version; otherwise, we retain the compatibility.
    
    ---------
    
    Co-authored-by: Daniel Lemire <daniel@lemire.me>

--- include/simdjson/generic/ondemand/document.h
+++ include/simdjson/generic/ondemand/document.h
@@ -6,6 +6,8 @@
 #include "simdjson/generic/ondemand/json_iterator.h"
 #endif // SIMDJSON_CONDITIONAL_INCLUDE
 
+#include "simdjson/deserialize.h"
+
 namespace simdjson {
 namespace SIMDJSON_IMPLEMENTATION {
 namespace ondemand {
@@ -178,7 +180,18 @@ public:
    * @returns A value of the given type, parsed from the JSON.
    * @returns INCORRECT_TYPE If the JSON value is not the given type.
    */
-  template<typename T> simdjson_inline simdjson_result<T> get() & noexcept {
+  template<typename T> simdjson_inline simdjson_result<T> get() &
+#ifdef __cpp_concepts
+    noexcept(tag_invocable<deserialize_tag, std::type_identity<T>, document&> ? nothrow_tag_invocable<deserialize_tag, std::type_identity<T>, document&> : true)
+#else
+    noexcept
+#endif
+  {
+#ifdef __cpp_concepts
+    if constexpr (tag_invocable<deserialize_tag, std::type_identity<T>, document&>) {
+      return deserialize(std::type_identity<T>{}, *this);
+    } else {
+#endif // __cpp_concepts
     // Unless the simdjson library or the user provides an inline implementation, calling this method should
     // immediately fail.
     static_assert(!sizeof(T), "The get method with given type is not implemented by the simdjson library. "
@@ -186,16 +199,27 @@ public:
       "int64_t, double, and bool. We recommend you use get_double(), get_bool(), get_uint64(), get_int64(), "
       " get_object(), get_array(), get_raw_json_string(), or get_string() instead of the get template."
       " You may also add support for custom types, see our documentation.");
+#ifdef __cpp_concepts
+      }
+#endif
   }
-  /** @overload template<typename T> simdjson_result<T> get() & noexcept */
-  template<typename T> simdjson_inline simdjson_result<T> get() && noexcept {
-    // Unless the simdjson library or the user provides an inline implementation, calling this method should
-    // immediately fail.
-    static_assert(!sizeof(T), "The get method with given type is not implemented by the simdjson library. "
-      "The supported types are ondemand::object, ondemand::array, raw_json_string, std::string_view, uint64_t, "
-      "int64_t, double, and bool. We recommend you use get_double(), get_bool(), get_uint64(), get_int64(), "
-      " get_object(), get_array(), get_raw_json_string(), or get_string() instead of the get template."
-      " You may also add support for custom types, see our documentation.");
+  /**
+   * @overload template<typename T> simdjson_result<T> get() & noexcept
+   *
+   * We disallow the use tag_invoke CPO on a moved document; it may create UB
+   * if user uses `ondemand::array` or `ondemand::object` in their custom type.
+   *
+   * The member function is still remains specialize-able for compatibility
+   * reasons, but we completely disallow its use when a tag_invoke customization
+   * is provided.
+   */
+  template<typename T>
+#ifdef __cpp_concepts
+    requires (!tag_invocable<deserialize_tag, std::type_identity<T>, document&>)
+#endif
+  simdjson_inline simdjson_result<T> get() && noexcept {
+      static_assert(!std::is_same_v<T, array> && !std::is_same_v<T, object>, "You should never hold either an ondemand::array or ondemand::object without a corresponding ondemand::document being alive; that would be Undefined Behaviour.");
+      return static_cast<document&>(*this).get<T>();
   }
 
   /**
