e422933414c563a82e031e0204def33064bfb070
Daniel Lemire <daniel@lemire.me>
2025-01-27 20:35:57 -0500

    release 3.12.0

--- include/simdjson/simdjson_version.h
+++ include/simdjson/simdjson_version.h
@@ -4,7 +4,7 @@
 #define SIMDJSON_SIMDJSON_VERSION_H
 
 /** The version of simdjson being used (major.minor.revision) */
-#define SIMDJSON_VERSION "3.11.6"
+#define SIMDJSON_VERSION "3.12.0"
 
 namespace simdjson {
 enum {
@@ -15,11 +15,11 @@ enum {
   /**
    * The minor version (major.MINOR.revision) of simdjson being used.
    */
-  SIMDJSON_VERSION_MINOR = 11,
+  SIMDJSON_VERSION_MINOR = 12,
   /**
    * The revision (major.minor.REVISION) of simdjson being used.
    */
-  SIMDJSON_VERSION_REVISION = 6
+  SIMDJSON_VERSION_REVISION = 0
 };
 } // namespace simdjson
 
