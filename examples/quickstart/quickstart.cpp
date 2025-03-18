005133c5553f0c6704c86fae1bbdf63915cae6c5
Joseph Olabisi <dabigjoe6@gmail.com>
2025-03-18 02:20:30 +0000

    wip - bruteforce surface wildcard with result

--- examples/quickstart/quickstart.cpp
+++ examples/quickstart/quickstart.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "simdjson.h"
+#include <vector>
 
 int main(void) {
   simdjson::dom::parser parser;
@@ -31,7 +32,25 @@ int main(void) {
   // std::cout << tweets["search_metadata"]["count"] << " results." << std::endl;
 
   simdjson::dom::element parsed_json = parser.parse(json_string);
-  std::cout << parsed_json["firstName"] << std::endl;
+  // std::cout << parsed_json["firstName"] << std::endl;
 
-  std::cout << parsed_json.at_path_new("$.*")[0] << std::endl;
+  auto result = parsed_json.at_path_new("$.*");
+  std::vector<simdjson::dom::element> values = result.value();
+
+  std::string string_result;
+
+  string_result = "[";
+  for (int i = 0; i < values.size(); ++i) {
+    simdjson::internal::string_builder<> sb;
+    sb.append(values[i]);
+    string_result = string_result += sb.str();
+    string_result += ",\n";
+  }
+  string_result += "]";
+
+  std::cout << string_result << std::endl;
+
+  // trying to figure out how printing arrays are handled
+  // auto result = parsed_json["phoneNumbers"];
+  // std::cout << result << std::endl;
 }
