5753f54f6cc30153a1b6f3b9853ca215dd65254b
Joseph Olabisi <dabigjoe6@gmail.com>
2025-07-21 21:31:16 +0100

    fix boundary check and unnecessary string allocation

--- include/simdjson/dom/object-inl.h
+++ include/simdjson/dom/object-inl.h
@@ -219,7 +219,9 @@ inline simdjson_result<std::vector<element>> object::at_path_with_wildcard(std::
         std::vector<element>::iterator child_values_begin = child_values.begin();
         std::vector<element>::iterator child_values_end = child_values.end();
 
-        process_json_path_of_child_elements(child_values_begin, child_values_end, "$" + std::string(json_path), result);
+        json_path = "$" + std::string(json_path);
+
+        process_json_path_of_child_elements(child_values_begin, child_values_end, json_path, result);
       }
 
       return result;
