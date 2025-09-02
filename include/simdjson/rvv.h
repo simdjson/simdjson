049ea612329ab0f16d651d67c81e4b525b397079
Cheng Zong <saberalter@hust.edu.cn>
2025-09-02 17:30:03 +0800

    WIP: initial RVV implementation
    Co-authored-by: gong-flying <gongxiaofei24@iscas.ac.cn>

---
+++ include/simdjson/rvv.h
@@ -0,0 +1,8 @@
+#ifndef SIMDJSON_RVV_H
+#define SIMDJSON_RVV_H
+
+#include "simdjson/rvv/begin.h"
+#include "simdjson/generic/amalgamated.h"
+#include "simdjson/rvv/end.h"
+
+#endif // SIMDJSON_RVV_H
