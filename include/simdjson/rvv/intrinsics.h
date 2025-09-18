ff1ad4c72a40d4063717b39596e389bfa4ea7ed0
Cheng Zong <saberalter@hust.edu.cn>
2025-09-18 13:25:58 +0800

    Fix riscv intrinsics header missing SIMDJSON_CONDITIONAL_INCLUDE guard
    Co-authored-by: gong-flying <gongxiaofei24@iscas.ac.cn>

--- include/simdjson/rvv/intrinsics.h
+++ include/simdjson/rvv/intrinsics.h
@@ -1,17 +1,16 @@
-#ifdef SIMDJSON_CONDITIONAL_INCLUDE
-
 #ifndef SIMDJSON_RVV_INTRINSICS_H
 #define SIMDJSON_RVV_INTRINSICS_H
 
 #ifndef SIMDJSON_CONDITIONAL_INCLUDE
 #include "simdjson/rvv/base.h"
-#endif
 
 #if defined(__riscv_vector)
 #include <riscv_vector.h>
 #else
 #error "RVV intrinsics header included but -march=riscv_v not enabled"
 #endif
+
+#endif // SIMDJSON_CONDITIONAL_INCLUDE
 
 // Get vector length in bytes at run-time (VLENB CSR)
 static inline size_t rvv_vlenb() {
@@ -25,5 +24,3 @@ static_assert(rvv_vlenb() * 4 <= simdjson::SIMDJSON_PADDING,
               "SIMDJSON_PADDING too small for RVV");
 
 #endif // SIMDJSON_RVV_INTRINSICS_H
-
-#endif // SIMDJSON_CONDITIONAL_INCLUDE
