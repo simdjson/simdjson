625adceb241a9a8e18869a1246f616f6233a6855
Daniel Lemire <daniel@lemire.me>
2025-09-26 21:29:28 -0400

    updating single-header

--- singleheader/simdjson.cpp
+++ singleheader/simdjson.cpp
@@ -1,4 +1,4 @@
-/* auto-generated on 2025-09-26 01:29:33 -0700. version 4.0.6 Do not edit! */
+/* auto-generated on 2025-09-26 21:04:53 -0400. version 4.0.6 Do not edit! */
 /* including simdjson.cpp:  */
 /* begin file simdjson.cpp */
 #define SIMDJSON_SRC_SIMDJSON_CPP
